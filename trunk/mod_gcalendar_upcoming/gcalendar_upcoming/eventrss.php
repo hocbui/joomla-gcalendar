8bc71d4eebcb8037997c63a29665f923621594eb
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-01-21 15:57:13 +0000

    initial

---
+++ trunk/mod_gcalendar_upcoming/gcalendar_upcoming/eventrss.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+* Google calendar upcoming events module
+* @author allon
+* @version $Revision: 1.1.0 $
+**/
+
+
+define ('HOSTNAME', 'http://www.google.com/'); //only allow google stuff to be grabbed
+$timeLimit = 0; //default to no time limit
+$maxResults = 5;
+
+if (isset($_GET['timeLimit'])){$timeLimit = $_GET['timeLimit'];}
+if (isset($_GET['gcal_feed'])){$path = $_GET['gcal_feed'];}
+if (isset($_GET['maxResults'])){$maxResults = $_GET['maxResults'];}
+
+
+$path = substr($path, 22, strlen($path)-22); //strip off the Http google.com part
+$calendarURL = HOSTNAME.$path;
+$today = date('Y-m-d');
+$endDate = mktime() + ($timeLimit * 2592000);
+$endDate = date('Y-m-d', $endDate) ;
+$calendarURL = $calendarURL."?start-min=".$today;
+if ($timeLimit > 0) { $calendarURL .= "&start-max=".$endDate; }
+$calendarURL .= "&max-results=".$maxResults."&orderby=starttime&sortorder=ascending";
+$calendarURL .= "&singleevents=true";
+
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $calendarURL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$feed = curl_exec($ch);
+curl_close($ch);
+
+header('Content-type: text/xml'); 
+echo $feed;
+
+?>
