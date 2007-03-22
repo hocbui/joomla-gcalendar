1c9fe29188ba81d50d97bf0dcbef6270ed1e6ad9
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-03-22 14:13:22 +0000

    issue 9: added some checks curl

--- trunk/mod_gcalendar_upcoming/gcalendar_upcoming/eventrss.php
+++ trunk/mod_gcalendar_upcoming/gcalendar_upcoming/eventrss.php
@@ -26,13 +26,34 @@ if ($timeLimit > 0) { $calendarURL .= "&start-max=".$endDate; }
 $calendarURL .= "&max-results=".$maxResults."&orderby=starttime&sortorder=ascending";
 $calendarURL .= "&singleevents=true";
 
-$ch = curl_init();
-$timeout = 5;
-curl_setopt($ch, CURLOPT_URL, $calendarURL);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-$feed = curl_exec($ch);
-curl_close($ch);
+if(function_exists('curl_init')){
+  $ch = curl_init();
+  if(!$ch){
+    $feed = file_get_contents($calendarURL);
+  }else{
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $calendarURL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $feed = curl_exec($ch);
+    
+    if(empty($feed))$feed = file_get_contents($calendarURL);
+    else{
+      $info = curl_getinfo($ch);
+      if(empty($info['http_code'])){
+        $feed = file_get_contents($calendarURL);
+      }else if(strstr($info['http_code'],4)||strstr($info['http_code'],5)){
+        //we have an error
+	$feed = file_get_contents($calendarURL);
+      }
+    }
+    
+    curl_close($ch);
+  }
+}
+else{
+  $feed = file_get_contents($calendarURL);
+}
 
 header('Content-type: text/xml'); 
 echo $feed;
