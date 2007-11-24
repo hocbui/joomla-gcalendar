6ee2886465e63c6ddafa8da66b2b6c6fb49d1a61
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-11-24 19:04:16 +0000

    some minor fixes for visual display

--- com_gcalendar/admin/install.sql
+++ com_gcalendar/admin/install.sql
@@ -1,10 +1,9 @@
-DROP TABLE IF EXISTS `#__gcalendar`;
 
-CREATE TABLE `#__gcalendar` (
+CREATE TABLE IF NOT EXISTS `#__gcalendar` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100) NOT NULL,
   `xmlUrl` varchar(100) NOT NULL,
   `htmlUrl` varchar(100) NOT NULL,
   `icalUrl` varchar(100) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=0;
