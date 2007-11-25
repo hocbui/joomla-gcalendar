3d012f4d045bc347b6cad88a7f8998ab309791cc
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-11-25 13:43:08 +0000

    migrated to joomla 1.5

--- mod_gcalendar_upcoming/mod_gcalendar_upcoming.php
+++ mod_gcalendar_upcoming/mod_gcalendar_upcoming.php
@@ -1,27 +1,13 @@
 <?php
 
-
 /**
 * Google calendar upcoming events module
 * @author allon
-* @version $Revision: 1.4.1 $
+* @version $Revision: 1.5.0 $
 **/
 
 // no direct access
-defined('_VALID_MOS') or die('Restricted access');
-global $mosConfig_lang;
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+require( JModuleHelper::getLayoutPath( 'mod_gcalendar_upcoming' ) );
 ?>
-
-<div id="st"></div>
-<div id="gcalajax"></div>
-<script language="JavaScript" type="text/javascript">
-  var calendarName = '<?php echo $params->get('name', '')?>';
-  var rootUrl = '<?php echo $mosConfig_live_site;?>';
-  var maxResults = '<?php echo $params->get('max', 5);?>';
-  var openInNewWindow = '<?php echo $params->get('openWindow', 5);?>';
-  var lang = '<?php echo $mosConfig_lang;?>';
-</script>
-
-<script src="<?php echo $mosConfig_live_site;?>/modules/gcalendar_upcoming/gcalajax.js" language="javascript" type="text/javascript">
-</script>
-
