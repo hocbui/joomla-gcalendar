0d67db4eec085508e50420070ea803f633cbcbe2
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-11-28 19:30:16 +0000

    support the raw view for the upcoming module

--- com_gcalendar/site/controller.php
+++ com_gcalendar/site/controller.php
@@ -22,6 +22,38 @@ class GCalendarController extends JController
 	{
 		parent::display();
 	}
+	
+	function content()
+	{
+		$document =& JFactory::getDocument();
+		
+		$viewType	= $document->getType();
+		$viewName	= JRequest::getCmd( 'view', 'Content' );
+		$viewLayout	= JRequest::getCmd( 'layout', 'default' );
+
+		$view = & $this->getView( $viewName, $viewType, '', array( 'base_path'=>$this->_basePath));
+
+		// Get/Create the model
+		if ($model = & $this->getModel('gcalendar')) {
+			// Push the model into the view (as default)
+			$model->setState('calendarName',JRequest::getVar('calendarName', null));
+			$model->setState('calendarType',JRequest::getVar('calendarType', null));
+			
+			$view->setModel($model, true);
+		}
+
+		// Set the layout
+		$view->setLayout($viewLayout);
+
+		// Display the view
+		if ($cachable) {
+			global $option;
+			$cache =& JFactory::getCache($option, 'view');
+			$cache->get($view, 'display');
+		} else {
+			$view->display();
+		}
+	}
 
 }
 ?>
