cbe579cb449d736c6a83af3a1546fe6000bb7d10
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-12-01 15:05:48 +0000

    it runs now, there is not anymore a lot of to do

--- mod_gcalendar_upcoming/tmpl/default.php
+++ mod_gcalendar_upcoming/tmpl/default.php
@@ -10,13 +10,19 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
 
-<div id="st"></div>
-<div id="gcalajax"></div>
+<div id="upcoming_events_content"></div>
 <script language="JavaScript" type="text/javascript">
+  var calendarName='<?php echo $params->get('name', '')?>';
   var rootUrl = '<?php echo JURI::base();?>';
   var maxResults = '<?php echo $params->get('max', 5);?>';
   var openInNewWindow = '<?php echo $params->get('openWindow', 1);?>';
-  var Backend = '<?php echo JURI::base()."index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&calendarName=".$params->get('name', '')?>';
+  var Backend = '<?php echo JURI::base()."index.php?option=com_gcalendar&task=content&format=raw&tmpl=component&calendarType=xmlUrl&calendarName=".$params->get('name', '')?>';
+  var checkingtext = '<?php echo JText::_( 'CHECK_EVENTS' );?>';
+  var noEventsText = '<?php echo JText::_( 'NO_EVENTS' );?>';
+  var errorText = '<?php echo JText::_( 'ERROR_RECEIVING_EVENTS' );?>';
+  var busyText = '<?php echo JText::_( 'BUSY_EVENT' );?>';
+  var df = '<?php echo $params->get('dateFormat', 'dd.mm.yyyy HH:MM');?>';
+  var dff = '<?php echo $params->get('dateFormatFull', 'dd.mm.yyyy');?>';
 </script>
 
 <script src="<?php echo JURI::base()."modules/mod_gcalendar_upcoming/tmpl/gcalendar.js"?>" language="javascript" type="text/javascript">
