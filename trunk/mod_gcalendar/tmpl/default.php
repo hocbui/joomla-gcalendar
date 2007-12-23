61ec80da14aa013e757133b570412b7e1086792f
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2007-12-23 10:57:00 +0000

    removed auto height, it is not working with urls others than the own host

--- trunk/mod_gcalendar/tmpl/default.php
+++ trunk/mod_gcalendar/tmpl/default.php
@@ -8,30 +8,7 @@
 
 defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 
-<script language="javascript" type="text/javascript">
-function iFrameHeightX( iFrameId ) {
-	var h = 0;
-	if ( !document.all ) {
-		h = document.getElementById(iFrameId).contentDocument.height;
-		document.getElementById(iFrameId).style.height = h + 60 + 'px';
-	} else if( document.all ) {
-		h = document.frames(iFrameId).document.body.scrollHeight;
-		document.all[iFrameId].style.height = h + 20 + 'px';
-	}
-}
-</script>
-
-<?php
-
-// auto height control
-if ( $params->def( 'height_auto' ) ) {
-	$load = 'onload="iFrameHeightX(\'blockrandom' . $mod_wrapper_count . '\')" ';
-} else {
-	$load = '';
-}
-?>
 <iframe
-<?php echo $load; ?>
 id="mod_gcalendar"
 src="<?php echo $calendar; ?>"
 width="<?php echo $params->get( 'width' ); ?>"
