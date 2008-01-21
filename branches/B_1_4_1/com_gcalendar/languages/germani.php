462766410a1b9987ba1217aebdb09e2ffee50e55
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-01-21 15:47:05 +0000

    added german language

---
+++ branches/B_1_4_1/com_gcalendar/languages/germani.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Google calendar component
+* @author allon
+* @version $Revision: 1.4.2 $
+**/
+
+// ensure this file is being included by a parent file
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+
+DEFINE("_GCALENDAR_READ_EVENTS_ERROR","Ihre Server Security verbietet das eröffnen von Verbindungen zu anderen Servern (allow_url_fopen ist ausgeschaltet). Bitte kontaktieren Sie Ihren Administrator.");
+?>
