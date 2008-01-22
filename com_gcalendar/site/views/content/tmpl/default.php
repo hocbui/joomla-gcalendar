b03d55ebf497f7923fc741c816f4d74828aff279
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-01-22 13:42:20 +0000

    added better error handling when no calendar is specified

--- com_gcalendar/site/views/content/tmpl/default.php
+++ com_gcalendar/site/views/content/tmpl/default.php
@@ -12,7 +12,7 @@ $timeLimit = JRequest::getVar('timeLimit',0); //default to no time limit
 $maxResults = JRequest::getVar('maxResults',5);
 $xmlType = JRequest::getVar('xmlType','full');
 
-if($this->calendarType === 'xmlUrl' && $xmlType==='full'){
+if(!empty($path) && $this->calendarType === 'xmlUrl' && $xmlType==='full'){
 	if(strpos($path,'public/full')===false){
 		$path=substr($path,0,strpos($path,'public')).'public/full';
 	}
@@ -25,14 +25,18 @@ if($this->calendarType === 'xmlUrl' && $xmlType==='full'){
 	$path .= "&max-results=".$maxResults."&orderby=starttime&sortorder=ascending";
 	$path .= "&singleevents=true";
 }
-
+	
 $allow_url_fopen = (bool) ini_get('allow_url_fopen');
-if($allow_url_fopen){
-  	$feed = file_get_contents($path);
-} else {
+if(!$allow_url_fopen){
 	$feed = '<?xml version="1.0" encoding="utf-8"?><content><error>';
 	$feed .= JText::_( 'READ_EVENTS_ERROR' );
 	$feed .= '</error></content>';
+} else if(empty($path)){
+	$feed = '<?xml version="1.0" encoding="utf-8"?><content><error>';
+	$feed .= JText::_( 'NO_CALENDAR_SPECIFIED' );
+	$feed .= '</error></content>';
+} else {
+  	$feed = file_get_contents($path);
 }
 if($this->calendarType==='xmlUrl') header('Content-type: text/xml');
 else if($this->calendarType==='icalUrl') header('Content-type: text/calendar');
