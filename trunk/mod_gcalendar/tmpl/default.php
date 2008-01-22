f7fd7075cd31ffc09373044ed8fe5c9154d7b155
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-01-22 13:57:53 +0000

    added better error handling when no calendar is specified

--- trunk/mod_gcalendar/tmpl/default.php
+++ trunk/mod_gcalendar/tmpl/default.php
@@ -6,16 +6,23 @@
 * @version $Revision: 1.5.0 $
 **/
 
-defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
 
-<iframe
-id="mod_gcalendar"
-src="<?php echo $calendar; ?>"
-width="<?php echo $params->get( 'width' ); ?>"
-height="<?php echo $params->get( 'height' ); ?>"
-scrolling="<?php echo $params->get( 'scrolling' ); ?>"
-align="top"
-frameborder="0"
-class="mod_gcalendar<?php echo $params->get( 'moduleclass_sfx' ); ?>">
-<?php echo JText::_( 'NO_IFRAMES' ); ?>
-</iframe>
+if(empty($calendar)){
+	echo JText::_( 'NO_CALENDAR' );
+}else{
+?>
+	<iframe
+	id="mod_gcalendar"
+	src="<?php echo $calendar; ?>"
+	width="<?php echo $params->get( 'width' ); ?>"
+	height="<?php echo $params->get( 'height' ); ?>"
+	scrolling="<?php echo $params->get( 'scrolling' ); ?>"
+	align="top"
+	frameborder="0"
+	class="mod_gcalendar<?php echo $params->get( 'moduleclass_sfx' ); ?>">
+	<?php echo JText::_( 'NO_IFRAMES' ); ?>
+	</iframe>
+	<?php
+}
+?>
