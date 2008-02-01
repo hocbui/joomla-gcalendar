fe49682a3052539e5fd33df50498d34e8a586e6a
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-02-01 21:01:49 +0000

    different time fixes

--- mod_gcalendar_latest/tmpl/gcalendar.js
+++ mod_gcalendar_latest/tmpl/gcalendar.js
@@ -24,7 +24,21 @@ else if (window.ActiveXObject){ // IE
     }
 }
 
-RSSRequestl();
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+addLoadEvent( RSSRequestl());
 
 /*
 * Main AJAX RSS reader request
@@ -171,8 +185,12 @@ function ReqChangel() {
 					}
                     
                     contentl+='<div>';
-                    if(!isAllDayl) contentl= contentl+ publishedl+" "+dateFormatl(itemTimel, dfl);
-                    else contentl = contentl+ publishedl+" "+dateFormatl(itemTimel, dffl);
+                    try {
+	                    if(!isAllDayl) contentl+= publishedl+" "+dateFormat(itemTimel, dfl);
+	                    else contentl+= publishedl+" "+dateFormat(itemTimel, dffl);
+                    } catch (e) {
+						contentl+=itemTimeXMLl;
+					}
                     
                     contentl+='</div>';
                     var linkl = 'href="'+backLinkl.replace('{eventPlace}',itemLinkl.substring(itemLinkl.indexOf('eid=')+4,itemLinkl.length)).replace('{ctzPlace}',timezonel)+'"';
@@ -195,112 +213,4 @@ function ReqChangel() {
 		}
 	}
 	
-}
-
-var dateFormatl = function () {
-	var	token        = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloZ]|"[^"]*"|'[^']*'/g,
-		timezone     = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
-		timezoneClip = /[^-+\dA-Z]/g,
-		pad = function (value, length) {
-			value = String(value);
-			length = parseInt(length) || 2;
-			while (value.length < length)
-				value = "0" + value;
-			return value;
-		};
-
-	// Regexes and supporting functions are cached through closure
-	return function (date, mask) {
-		// Treat the first argument as a mask if it doesn't contain any numbers
-		if (
-			arguments.length == 1 &&
-			(typeof date == "string" || date instanceof String) &&
-			!/\d/.test(date)
-		) {
-			mask = date;
-			date = undefined;
-		}
-
-		date = date ? new Date(date) : new Date();
-		if (isNaN(date))
-			throw "invalid date";
-
-		var dF = dateFormat;
-		mask   = String(dF.masks[mask] || mask || dF.masks["default"]);
-
-		var	d = date.getDate(),
-			D = date.getDay(),
-			m = date.getMonth(),
-			y = date.getFullYear(),
-			H = date.getHours(),
-			M = date.getMinutes(),
-			s = date.getSeconds(),
-			L = date.getMilliseconds(),
-			o = date.getTimezoneOffset(),
-			flags = {
-				d:    d,
-				dd:   pad(d),
-				ddd:  dF.i18n.dayNames[D],
-				dddd: dF.i18n.dayNames[D + 7],
-				m:    m + 1,
-				mm:   pad(m + 1),
-				mmm:  dF.i18n.monthNames[m],
-				mmmm: dF.i18n.monthNames[m + 12],
-				yy:   String(y).slice(2),
-				yyyy: y,
-				h:    H % 12 || 12,
-				hh:   pad(H % 12 || 12),
-				H:    H,
-				HH:   pad(H),
-				M:    M,
-				MM:   pad(M),
-				s:    s,
-				ss:   pad(s),
-				l:    pad(L, 3),
-				L:    pad(L > 99 ? Math.round(L / 10) : L),
-				t:    H < 12 ? "a"  : "p",
-				tt:   H < 12 ? "am" : "pm",
-				T:    H < 12 ? "A"  : "P",
-				TT:   H < 12 ? "AM" : "PM",
-				Z:    (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
-				o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4)
-			};
-
-		return mask.replace(token, function ($0) {
-			return ($0 in flags) ? flags[$0] : $0.slice(1, $0.length - 1);
-		});
-	};
-}();
-
-// Some common format strings
-dateFormatl.masks = {
-	"default":       "ddd mmm d yyyy HH:MM:ss",
-	shortDate:       "m/d/yy",
-	mediumDate:      "mmm d, yyyy",
-	longDate:        "mmmm d, yyyy",
-	fullDate:        "dddd, mmmm d, yyyy",
-	shortTime:       "h:MM TT",
-	mediumTime:      "h:MM:ss TT",
-	longTime:        "h:MM:ss TT Z",
-	isoDate:         "yyyy-mm-dd",
-	isoTime:         "HH:MM:ss",
-	isoDateTime:     "yyyy-mm-dd'T'HH:MM:ss",
-	isoFullDateTime: "yyyy-mm-dd'T'HH:MM:ss.lo"
-};
-
-// Internationalization strings
-dateFormatl.i18n = {
-	dayNames:   [
-		"Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat",
-		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-	],
-	monthNames: [
-		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-		"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-	]
-};
-
-// For convenience...
-Date.prototype.formatl = function (mask) {
-	return dateFormatl(this, mask);
 }
