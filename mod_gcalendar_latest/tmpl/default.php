816250d62818d38b7375a968fb240586ce7f57d9
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-02-01 21:41:38 +0000

    names are decoded

--- mod_gcalendar_latest/tmpl/default.php
+++ mod_gcalendar_latest/tmpl/default.php
@@ -13,7 +13,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <div id="latest_events_content"></div>
 <script language="JavaScript" type="text/javascript">
   var openInNewWindowl = '<?php echo $params->get('openWindow', 1);?>';
-  var Backendl = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=basic&calendarName=".$params->get('name_latest', '')."&maxResults=".$params->get('max', 5),false);?>';
+  var Backendl = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=basic&calendarName=".urlencode($params->get('name_latest', ''))."&maxResults=".$params->get('max', 5),false);?>';
   var backLinkl = '<?php echo urldecode(JRoute::_("index.php?option=com_gcalendar&task=event&eventID={eventPlace}&calendarName=".$params->get('name_latest', '')."&ctz={ctzPlace}"));?>';
   var checkingtextl = '<?php echo JText::_( 'CHECK_EVENTS' );?>';
   var noEventsTextl = '<?php echo JText::_( 'NO_EVENTS' );?>';
--- mod_gcalendar_upcoming/tmpl/default.php
+++ mod_gcalendar_upcoming/tmpl/default.php
@@ -13,7 +13,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <div id="upcoming_events_content"></div>
 <script language="JavaScript" type="text/javascript">
   var openInNewWindow = '<?php echo $params->get('openWindow', 1);?>';
-  var Backend = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=full&calendarName=".$params->get('name', '')."&maxResults=".$params->get('max', 5),false);?>';
+  var Backend = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=full&calendarName=".urlencode($params->get('name', ''))."&maxResults=".$params->get('max', 5),false);?>';
   var backLink = '<?php echo urldecode(JRoute::_("index.php?option=com_gcalendar&task=event&eventID={eventPlace}&calendarName=".$params->get('name', '')."&ctz={ctzPlace}"));?>';
   var checkingtext = '<?php echo JText::_( 'CHECK_EVENTS' );?>';
   var noEventsText = '<?php echo JText::_( 'NO_EVENTS' );?>';
