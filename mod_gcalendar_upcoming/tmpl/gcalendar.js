8af4448de83ab974c1a864f1ac3fb990b7fd964c
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-02-06 08:33:42 +0000

    removed onload workaround

--- mod_gcalendar_upcoming/tmpl/gcalendar.js
+++ mod_gcalendar_upcoming/tmpl/gcalendar.js
@@ -24,21 +24,7 @@ else if (window.ActiveXObject){ // IE
     }
 }
 
-function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      if (oldonload) {
-        oldonload();
-      }
-      func();
-    }
-  }
-}
-
-addLoadEvent( RSSRequest());
+RSSRequest();
 
 /*
 * Main AJAX RSS reader request
