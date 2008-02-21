a484462bacd7c399610d4c43acb7921c20ac41de
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-02-21 07:47:01 +0000

    removed errors if backend call is not like mos_config_live_site

--- branches/B_1_4_1/mod_gcalendar_latest/mod_gcalendar_latest.php
+++ branches/B_1_4_1/mod_gcalendar_latest/mod_gcalendar_latest.php
@@ -21,8 +21,8 @@ if (file_exists($mosConfig_absolute_path."/modules/mod_gcalendar_latest/language
 <div id="latest_events_content"></div>
 <script language="JavaScript" type="text/javascript">
   var openInNewWindowl = '<?php echo $params->get('openWindow', 0);?>';
-  var Backendl = '<?php echo $mosConfig_live_site."/index2.php?option=com_gcalendar&task=content&no_html=1&calendarType=xmlUrl&xmlType=basic&calendarName=".urlencode($params->get('name_latest', ''))."&maxResults=".$params->get('max', 5);?>';
-  var backLinkl = '<?php echo $mosConfig_live_site.urldecode("/index.php?option=com_gcalendar&eventID={eventPlace}&name=".$params->get('name_latest', '')."&ctz={ctzPlace}");?>';
+  var Backendl = '<?php echo "index2.php?option=com_gcalendar&task=content&no_html=1&calendarType=xmlUrl&xmlType=basic&calendarName=".urlencode($params->get('name_latest', ''))."&maxResults=".$params->get('max', 5);?>';
+  var backLinkl = '<?php echo urldecode("index.php?option=com_gcalendar&eventID={eventPlace}&name=".$params->get('name_latest', '')."&ctz={ctzPlace}");?>';
   var checkingtextl = '<?php echo _GCALENDAR_LATEST_CHECK_EVENTS;?>';
   var noEventsTextl = '<?php echo _GCALENDAR_LATEST_NO_EVENTS;?>';
   var busyTextl = '<?php echo _GCALENDAR_LATEST_BUSY_EVENT;?>';
