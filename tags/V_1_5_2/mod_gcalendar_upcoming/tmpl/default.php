d3a85c51cb3697f92658eb766e12fd43e197b3d0
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-03-13 18:42:45 +0000

    tagged as release 1.5.2

---
+++ tags/V_1_5_2/mod_gcalendar_upcoming/tmpl/default.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* Google calendar upcoming events module
+* @author allon
+* @version $Revision: 1.5.2 $
+**/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+
+<div id="upcoming_events_content"></div>
+<script language="JavaScript" type="text/javascript">
+  // <![CDATA[
+  var openInNewWindow = '<?php echo $params->get('openWindow', 1);?>';
+  var Backend = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=full&calendarName=".urlencode($params->get('name', ''))."&maxResults=".$params->get('max', 5),false);?>';
+  var backLink = '<?php echo urldecode(JRoute::_("index.php?option=com_gcalendar&task=event&eventID={eventPlace}&calendarName=".$params->get('name', '')."&ctz={ctzPlace}"));?>';
+  var checkingtext = '<?php echo JText::_( 'CHECK_EVENTS' );?>';
+  var noEventsText = '<?php echo JText::_( 'NO_EVENTS' );?>';
+  var busyText = '<?php echo JText::_( 'BUSY_EVENT' );?>';
+  var df = '<?php echo $params->get('dateFormat', 'dd.mm.yyyy HH:MM');?>';
+  var dff = '<?php echo $params->get('dateFormatFull', 'dd.mm.yyyy');?>';
+  var showEndDate = '<?php echo $params->get('showEndDate', 0);?>';
+  // ]]>
+</script>
+
+<script src="<?php echo JURI::base()."modules/mod_gcalendar_upcoming/tmpl/date.format.js"?>" language="javascript" type="text/javascript">
+</script>
+<script src="<?php echo JURI::base()."modules/mod_gcalendar_upcoming/tmpl/gcalendar.js"?>" language="javascript" type="text/javascript">
+</script>
