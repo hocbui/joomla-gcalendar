5ded75ead491d3dce0b306eb276d354a37095fb0
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-11-28 08:36:57 +0000

    changing from ajax rss to simplepie as rss reader

--- trunk/mod_gcalendar_latest/mod_gcalendar_latest.php
+++ trunk/mod_gcalendar_latest/mod_gcalendar_latest.php
@@ -9,5 +9,10 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
+// Include the helper functions only once
+require_once (dirname(__FILE__).DS.'helper.php');
+// Get data from helper class
+$gcalendar_data = modGcalendarLatestHelper::getFeed($params);
+
 require( JModuleHelper::getLayoutPath( 'mod_gcalendar_latest' ) );
 ?>
--- trunk/mod_gcalendar_latest/tmpl/default.php
+++ trunk/mod_gcalendar_latest/tmpl/default.php
@@ -8,25 +8,51 @@
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
+
+//Time offset - if times are appearing too early or too late on your website, change this.
+$offset="now"; // you can use "+1 hour" here for example
+
+// How you want each thing to display.
+// By default, this contains all the bits you can grab. You can put ###DATE### in here too if
+// you want to, and disable the 'group by date' below.
+// $event_display="<P><B>###TITLE###</b> - published ###PUBLISHED### (<a href='###LINK###'>add this</a>)<BR>###WHERE### (<a href='###MAPLINK###'>map</a>)<br>###DESCRIPTION###</p>";
+$event_display="<p>###PUBLISHED###<br><a href='###LINK###'>###TITLE###</a></p>";
+
+
+// The separate date header is here
+$event_dateheader="<P><B>###DATE###</b></P>";
+$GroupByDate=true;
+// Change the above to 'false' if you don't want to group this by dates,
+// but remember to add ###DATE### in the event_display if you do.
+
+// ...and how many you want to display (leave at 999 for everything)
+$items_to_show=5;
+
+// Loop through the (now sorted) array, and display what we wanted.
+foreach ($gcalendar_data as $item) {
+    //Make any URLs used in the description also clickable: thanks Adam
+    $item['description'] = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item['description']);
+
+    // Now, let's run it through some str_replaces, and store it with the date for easy sorting later
+    $temp_event=$event_display;
+    $temp_dateheader=$event_dateheader;
+    $temp_event=str_replace("###TITLE###",$item['title'],$temp_event);
+    $temp_event=str_replace("###DESCRIPTION###",$item['description'],$temp_event);
+    $temp_event=str_replace("###DATE###",$item['published'],$temp_event);
+    $temp_dateheader=str_replace("###DATE###",$item['published'],$temp_dateheader);
+    $temp_event=str_replace("###PUBLISHED###",$item['published'],$temp_event);
+    $temp_event=str_replace("###WHERE###",$item['where'],$temp_event);
+    $temp_event=str_replace("###LINK###",urldecode(JRoute::_('index.php?option=com_gcalendar&task=event&eventID='.$item['id'].'&calendarName='.'joomla'.'&ctz=America/Chicago')),$temp_event);
+    $temp_event=str_replace("###MAPLINK###","http://maps.google.com/?q=".urlencode($item['where']),$temp_event);
+    // Accept and translate HTML
+    $temp_event=str_replace("&lt;","<",$temp_event);
+    $temp_event=str_replace("&gt;",">",$temp_event);
+    $temp_event=str_replace("&quot;","\"",$temp_event);
+
+    if (($items_to_show>0 AND $items_shown<$items_to_show)) {
+                if ($GroupByDate) {if ($gCalDate!=$old_date) { echo $temp_dateheader; $old_date=$gCalDate;}}
+        echo $temp_event;
+        $items_shown++;
+    }
+}
 ?>
-
-<div id="latest_events_content"></div>
-<script language="JavaScript" type="text/javascript">
-  // <![CDATA[
-  var openInNewWindowl = '<?php echo $params->get('openWindow', 1);?>';
-  var Backendl = '<?php echo JRoute::_("index.php?option=com_gcalendar&task=content&format=raw&calendarType=xmlUrl&xmlType=basic&calendarName=".urlencode($params->get('name_latest', ''))."&maxResults=".$params->get('max', 5),false);?>';
-  var backLinkl = '<?php echo urldecode(JRoute::_("index.php?option=com_gcalendar&task=event&eventID={eventPlace}&calendarName=".$params->get('name_latest', '')."&ctz={ctzPlace}"));?>';
-  var checkingtextl = '<?php echo JText::_( 'CHECK_EVENTS' );?>';
-  var noEventsTextl = '<?php echo JText::_( 'NO_EVENTS' );?>';
-  var busyTextl = '<?php echo JText::_( 'BUSY_EVENT' );?>';
-  var publishedl = '<?php echo JText::_( 'PUBLISHED' );?>';
-  var dfl = '<?php echo $params->get('dateFormat', 'dd.mm.yyyy HH:MM');?>';
-  var dffl = '<?php echo $params->get('dateFormatFull', 'dd.mm.yyyy');?>';
-  var showEndDatel = '<?php echo $params->get('showEndDate', 0);?>';
-  // ]]>
-</script>
-
-<script src="<?php echo JURI::base()."modules/mod_gcalendar_latest/tmpl/date.format.js"?>" language="javascript" type="text/javascript">
-</script>
-<script src="<?php echo JURI::base()."modules/mod_gcalendar_latest/tmpl/gcalendar.js"?>" language="javascript" type="text/javascript">
-</script>
