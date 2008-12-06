4710fbab2bb3d7fcbdc4046dfa3295e106596ae0
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-06 12:07:02 +0000

    Issue 85: runs now on simplepie

--- branches/Joomla_1_0_x/mod_gcalendar_upcoming/mod_gcalendar_upcoming/languages/english.php
+++ branches/Joomla_1_0_x/mod_gcalendar_upcoming/mod_gcalendar_upcoming/languages/english.php
@@ -9,7 +9,4 @@
 // ensure this file is being included by a parent file
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-DEFINE("_GCALENDAR_UPCOMING_CHECK_EVENTS","Receiving events.");
-DEFINE("_GCALENDAR_UPCOMING_NO_EVENTS","No events found.");
-DEFINE("_GCALENDAR_UPCOMING_BUSY_EVENT","Busy");
 ?>
