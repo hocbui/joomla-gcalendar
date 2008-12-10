53a7c722eae2de0f81ebb991f1827b8f13fca61b
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-10 11:02:23 +0000

    creating tag 2.0.0 RC2

---
+++ tags/V_2_0_0_RC2/Joomla_1_5_x/com_gcalendar/site/views/gcalendar/view.html.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Google calendar component
+ * @author allon
+ * @version $Revision: 2.0.0 $
+ */
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * HTML View class for the GCalendar Component
+ *
+ */
+class GCalendarViewGCalendar extends JView
+{
+	function display($tpl = null)
+	{
+		global $mainframe;
+		
+		$gcalendar = $this->get( 'GCalendar' );
+		$this->assignRef( 'gcalendar',	$gcalendar );
+		
+		$params = &$mainframe->getParams();
+		$this->assignRef('params'  , $params);
+
+		parent::display($tpl);
+	}
+}
+?>
