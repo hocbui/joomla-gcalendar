03e694275995276a1b3e3f2aaada6c7753bed931
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-13 11:18:01 +0000

    better error reporting

--- Joomla_1_0_x/mod_gcalendar_upcoming/mod_gcalendar_upcoming/languages/english.php
+++ Joomla_1_0_x/mod_gcalendar_upcoming/mod_gcalendar_upcoming/languages/english.php
@@ -9,5 +9,7 @@
 // ensure this file is being included by a parent file
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-DEFINE("_GCALENDAR_UPCOMING_NO_CALENDAR_SPECIFIED","There is no calendar specified!! Please configure in your Backend the correct calendar name.");
+DEFINE("_GCALENDAR_UPCOMING_CALENDAR_NO_DEFINED","There is no calendar specified!! Please set in the module parameter section a valid calendar name configured in the gcalendar component.");
+DEFINE("_GCALENDAR_UPCOMING_CALENDAR_NOT_FOUND","The calendar was not found in the database. The calendar name is: <br>");
+DEFINE("_GCALENDAR_UPCOMING_SP_ERROR","Simplepie detected an error. Please run the <a href=\"modules/mod_gcalendar_upcoming/sp_compatibility_test.php\">compatibility utility</a>.<br>The following Simplepie error occurred:<br>");
 ?>
