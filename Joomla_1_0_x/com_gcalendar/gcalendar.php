b3f7deb738588387d86d877d1515fab9653ee5c8
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-14 09:49:40 +0000

    Issue 74: added the hl parameter to the iframe link when showing the calendar event. the parameter is filled with the joomla variable _LANGUAGE

--- Joomla_1_0_x/com_gcalendar/gcalendar.php
+++ Joomla_1_0_x/com_gcalendar/gcalendar.php
@@ -11,7 +11,6 @@ defined('_VALID_MOS') or die('Restricted access');
 
 /** load the html drawing class */
 require_once ($mainframe->getPath('front_html'));
-require_once ( $mosConfig_absolute_path."/components/com_gcalendar/gcalendar.xml.php");
 
 // Get the right language if it exists
 if (file_exists($mosConfig_absolute_path."/components/com_gcalendar/languages/".$mosConfig_lang.".php")){
@@ -24,21 +23,16 @@ switch ( $task ) {
 	case 'view':
 		showCalendar($option);
 		break;
-	case 'content':
-		showContent($option);
+	case 'event':
+		showEvent($option);
 		break;
 	default:
 		showCalendar($option);
 		break;
 }
 
-function showContent($option){
-	XML_gcalendar :: displayCalendar();
-}
-
-
-function showCalendar($option) {
-	global $database, $Itemid, $mainframe, $mosConfig_lang;
+function showEvent($option){
+	global $database, $mainframe, $mosConfig_lang;
 
 	$menu = $mainframe->get('menu');
 	$params = new mosParameters($menu->params);
@@ -46,33 +40,57 @@ function showCalendar($option) {
 	$params->def('pageclass_sfx', '');
 	$params->def('height', '500');
 	$params->def('width', '100%');
-	$params->def('htmlUrl', mosGetParam($_REQUEST, 'page', ''));
+
+	//if we are called from a link in a gcalendar module
+	$eventID = mosGetParam($_REQUEST, 'eventID', '');
+	if (!empty($eventID)) {
+		$query = "select id,htmlUrl from #__gcalendar where name='.$name.'";
+		$database->setQuery($query);
+		$results = $database->loadObjectList();
+		if(!empty($reults)){
+			foreach ($results as $result) {
+				$params->set('htmlUrl', $result->htmlUrl);
+			}
+		}
+		$htmlUrl = $params->get('htmlUrl','');
+		if(empty($htmlUrl))
+			$params->set('htmlUrl', 'http://www.google.com');
+		$p= parse_url($params->get('htmlUrl',''));
+		
+		$timezone = mosGetParam($_REQUEST, 'ctz', '');
+		$lg = _LANGUAGE;
+		if(!empty($timezone))$timezone='&ctz='.$timezone;
+		if(!empty($lg))$lg='&hl='.$lg;
+		$params->set('htmlUrl', $p['scheme'].'://'.$p['host'].'/calendar/event?eid='.$eventID.$timezone.$lg);
+	}
+
+	HTML_gcalendar :: displayCalendar($params, $menu);
+}
+
+
+function showCalendar($option) {
+	global $database, $mainframe, $mosConfig_lang;
+
+	$menu = $mainframe->get('menu');
+	$params = new mosParameters($menu->params);
+	$params->def('scrolling', 'auto');
+	$params->def('pageclass_sfx', '');
+	$params->def('height', '500');
+	$params->def('width', '100%');
 
 	$name = $params->def('name', '');
-	if(mosGetParam($_REQUEST, 'name', '')!='')
-		$name=mosGetParam($_REQUEST, 'name', '');
-	if ($params->get('htmlUrl', '') == '') {
-		$database->setQuery("select id,htmlUrl from #__gcalendar where name='$name'");
-		$results = $database->loadObjectList();
+	$query = "select id,htmlUrl from #__gcalendar where name='$name'";
+	$database->setQuery($query);
+	$results = $database->loadObjectList();
+	if(!empty($results)){
 		foreach ($results as $result) {
 			$params->set('htmlUrl', $result->htmlUrl);
 		}
-
-		//if we are called from a link in a gcalendar module
-		$eventID = mosGetParam($_REQUEST, 'eventID', '');
-		if ($eventID != '') {
-			if($params->get('htmlUrl','')=='')
-				$params->set('htmlUrl', 'http://www.google.com');
-			$p= parse_url($params->get('htmlUrl',''));
-			
-			$timezone = mosGetParam($_REQUEST, 'ctz', '');
-			if($timezone != '')$timezone='&ctz='.$timezone;
-			$params->set('htmlUrl', $p['scheme'] . '://' . $p['host'] . '/calendar/event?eid=' . $eventID . $timezone);
-		}
+		
+		$mainframe->SetPageTitle($menu->name);
+		HTML_gcalendar :: displayCalendar($params, $menu);
+	}else{
+		echo _GCALENDAR_COMPONENT_NO_CALENDAR;
 	}
-
-	$mainframe->SetPageTitle($menu->name);
-
-	HTML_gcalendar :: displayCalendar($params, $menu);
 }
 ?>
--- Joomla_1_0_x/com_gcalendar/languages/english.php
+++ Joomla_1_0_x/com_gcalendar/languages/english.php
@@ -9,6 +9,5 @@
 // ensure this file is being included by a parent file
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-DEFINE("_GCALENDAR_READ_EVENTS_ERROR","Your server security doesn't allow connections to remote server (allow_url_fopen is turned off). Please contact the administrator.");
-DEFINE("_GCALENDAR_NO_CALENDAR_SPECIFIED","There is no calendar specified!! Please configure in your Backend the correct calendar name.");
+DEFINE("_GCALENDAR_COMPONENT_NO_CALENDAR","There is no calendar specified!! Please configure in your Backend the correct calendar name.");
 ?>
