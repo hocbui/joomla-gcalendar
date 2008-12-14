5e98cd6bf705d558e89b8ea9a5c0723e2064c5a4
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-14 09:49:40 +0000

    Issue 74: added the hl parameter to the iframe link when showing the calendar event. the parameter is filled with the joomla variable _LANGUAGE

--- trunk/Joomla_1_0_x/com_gcalendar/languages/german.php
+++ trunk/Joomla_1_0_x/com_gcalendar/languages/german.php
@@ -9,6 +9,5 @@
 // ensure this file is being included by a parent file
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-DEFINE("_GCALENDAR_READ_EVENTS_ERROR","Ihre Server Security verbietet das er�ffnen von Verbindungen zu anderen Servern (allow_url_fopen ist ausgeschaltet). Bitte kontaktieren Sie Ihren Administrator.");
-DEFINE("_GCALENDAR_NO_CALENDAR_SPECIFIED","Kein Kalender gefunden!! Bitte �berpr�fen Sie ob im Administrationsbereich ein Kalender angegeben wurde.");
+DEFINE("_GCALENDAR_COMPONENT_NO_CALENDAR","Kein Kalender gefunden!! Bitte �berpr�fen Sie ob im Administrationsbereich ein Kalender angegeben wurde.");
 ?>
