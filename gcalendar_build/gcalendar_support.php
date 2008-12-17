51be169276a3b820dd14676bfbd7e54f4672693e
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-17 05:47:49 +0000

    added a new support page

---
+++ gcalendar_build/gcalendar_support.php
@@ -0,0 +1,36 @@
+<table><tr><td valign="top">
+<h4>GCalendar Help</h4>
+
+<p>GCalendar is an extension for joomla based web sites which 
+supports a smooth integration of your google calendar.</p>
+
+<h4>Donation</h4>
+There is more effort behind GCalendar than you think... 
+<script type="text/javascript" src="http://www.ohloh.net/projects/26740/widgets/project_thin_badge"></script>
+<br><br>
+Because the sources are free the project depends on donations to support furthure 
+releases!! Please make a small donation with paypal.....<br><br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="302238">
+<input type="image" src="https://www.paypal.com/en_US/CH/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+<h4>Documentation and Support</h4>
+At <a href="http://gcalendar.allon.ch" target="_blank">gcalendar.allon.ch</a> you will find all the informations about
+the project as well as a forum to post questions.
+</td>
+<td  valign="top">
+<script type="text/javascript"><!--
+google_ad_client = "pub-4081656028076034";
+/* gcalendar support */
+google_ad_slot = "0189053471";
+google_ad_width = 120;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</td></tr></table>
