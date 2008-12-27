5995c3041f9bea56a89d64d777759c9a6c69d481
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2008-12-27 15:01:20 +0000

    has now more logical methodes

--- trunk/simplepie-gcalendar/demo/index.php
+++ trunk/simplepie-gcalendar/demo/index.php
@@ -1,6 +1,5 @@
 <?php
-$url = $_GET["feedurl"];
-$feed_type = $_GET["feedtype"];
+$url = $_POST["feedurl"];
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -8,18 +7,11 @@ $feed_type = $_GET["feedtype"];
 </head>
 <body>
 <h1>Simplepie Google Calendar demo web site</h1>
-	<form name="input" action="index.php" method="get">
+	<form name="input" action="index.php" method="post">
 <table>
 <tr>
 <td>Feed url:</td>
 <td><input type="text" name="feedurl" size="100"></td>
-</tr>
-<tr>
-<td>Feed type:</td>
-<td><SELECT NAME="feedtype">
-	<OPTION VALUE="basic" selected>Basic
-	<OPTION VALUE="full">Full	
-</SELECT></td>
 </tr>
 <tr><td><input type="submit" value="Submit"></td></tr>
 </table>
@@ -31,32 +23,32 @@ require_once ('simplepie.inc');
 require_once ('simplepie-gcalendar.php');
 
 $feed = new SimplePie_GCalendar();
-$feed->enable_cache('false');
-$feed->set_calendar_type($feed_type);
+$feed->set_show_past_events(FALSE);
+$feed->set_sort_ascending(TRUE);
+$feed->set_orderby_by_start_date(TRUE);
+$feed->set_expand_single_events(TRUE);
 
-$url = SimplePie_GCalendar::cfg_feed_without_past_events($url);
-if($feed->get_calendar_type() == 'full')
-	$url = SimplePie_GCalendar::ensure_feed_is_full($url);
 $feed->set_feed_url($url);
 
 if(!$content){
-	$feed->enable_order_by_date(false);
+	$feed->enable_order_by_date(FALSE);
 	$feed->init();
 	
 	$feed->handle_content_type();
-	$gcalendar_data = $feed->get_calendar_items();
+	$gcalendar_data = $feed->get_items();
+	echo '<p>feed url: '.$feed->feed_url.'</p>';
+	
 	for ($i = 0; $i < sizeof($gcalendar_data) ; $i++){
 		$item = $gcalendar_data[$i];
-		$gCalDate = date("d.m.Y H:i", $item->get_publish_date());
-		if($feed->get_calendar_type() == 'full'){
-			$gCalDate = date("d.m.Y H:i", $item->get_start_time());
-		}
+		$gCalDate = date("d.m.Y H:i", $item->get_start_time());
 		echo '<p>'.$gCalDate.'<br>'.$item->get_title().'<br>'.$item->get_description().'<hr></p>';
 	}
 }else{
-	//header("content-Type: text/text"); 
-	$feed = file_get_contents($url);
-	echo $feed;
+	//header("content-Type: text/text");
+	$content = '<font>';
+	$content .= file_get_contents($url);
+	$content .= '</font>';
+	echo $content;
 }
 ?>
 </body></html>
