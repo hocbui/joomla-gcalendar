6c76a5b1de6ec4923c6b8759e6063262c18e9b3e
stephenhodgman <stephenhodgman@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2009-01-09 01:56:56 +0000

    Fixed default.php to match changes made by Allon. Added param for option to show Event Title in bold face.

--- Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
+++ Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
@@ -10,8 +10,8 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 if(!empty($error)){
-	echo $error;
-	return;
+    echo $error;
+    return;
 }
 
 //  smh - 2008-12-17 - integer count of seconds in a day
@@ -23,42 +23,52 @@ $SECSINDAY=86400;
 // ###TITLE###, ###DESCRIPTION###, ###DATE###, ###FROM###, ###UNTIL###,
 // ###WHERE###, ###BACKLINK###, ###LINK###, ###MAPLINK###
 // You can put ###DATE### in here too if you want to, and disable the 'group by date' below.
-$dsplLink = '###BACKLINK###';
+$dsplLink = "<a href='###BACKLINK###'>###TITLE###</a>";
 if($params->get( 'openWindow', 0 )==1)
-	$dsplLink = '###LINK###';
-$event_display="<p>###DATE### ###FROM###<br><a href='".$dsplLink."'>###TITLE###</a></p>";
+        $dsplLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
+$event_display="<p>###DATE### ###FROM###<br>".$dsplLink."</p>";
 
 // smh 2009-01-08 Added params for underline between events and if link is active
 $dsplUnderline = '';
-if($params->get( 'showUnderline', 0) == 1)
+if($params->get( 'showUnderline', 0) == 1){
    $dsplUnderline = "<HR>";
-  
+}
 // dsplTitleLink determines if the Title will be clickable or not
 $dsplTitleLink = "###TITLE###";
 if ($params->get( 'showLink', 0)==1) {
-$dsplTitleLink = "<a href='".$dsplLink."'>###TITLE###</a>";  
+   if($params->get( 'openWindow', 0 )==1) {
+        $dsplTitleLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
+   } else {
+        $dsplTitleLink = "<a href='###BACKLINK###'>###TITLE###</a>";
 }
-// /smh 2009-01-08
+// Added option for a bolded title
+$displTitle = $dsplTitleLink;
+if ($params->get( 'boldTitle', 0)==1) {
+   $dsplTitle = "<strong>".$dsplTitleLink."</strong>";  
+}
 
-// The separate date header is here
-$event_dateheader="<P><B>###DATE###</b></P>";
-$GroupByDate=false;
-// Change the above to 'false' if you don't want to group this by dates,
-// but remember to add ###DATE### in the event_display if you do.
+// /smh 2009-01-08
 
 // Date format you want your details to appear
 $dateformat=$params->get('dateFormat', 'd.m.Y'); // 10 March 2009 - see http://www.php.net/date for details
 $timeformat=$params->get('timeFormat', 'H:i');; // 12.15am
+$calName = $params->get( 'name', NULL );
 
 // Loop through the array, and display what we wanted.
 for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++){
-	$item = $gcalendar_data[$i];
-	
-	// These are the dates we'll display
-    $gCalDate = date($dateformat, $item['startdate']);
-    $gCalStartTime = date($timeformat, $item['startdate']);
-    $gCalEndTime = date($timeformat, $item['enddate']);
-    //
+        $item = $gcalendar_data[$i];
+        
+        $tz = $params->get('timezone', '');
+        if($tz == ''){
+                $feed = $item->get_feed();
+                $tz = $feed->get_timezone();
+        }
+
+        // These are the dates we'll display
+    $startDate = date($dateformat, $item->get_start_time());
+    $startTime = date($timeformat, $item->get_start_time());
+    $endTime = date($timeformat, $item->get_end_time());
+    
     // smh 2008-12-17
     //       - modification to allow flexible output for various event types viz:
     //       Part of a day
@@ -77,7 +87,7 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
          // Single day, whole day	
        // smh 2009-01-08
          //$event_display="<p style=\"font-size: 90%;\">###DATE###</p><div><strong>###TITLE###</strong>".$dsplUnderline."</div>";
-         $event_display="<p style=\"font-size: 90%;\">###DATE###</p><p><strong>".$dsplTitleLink."</strong>".$dsplUnderline."</p>";
+         $event_display="<p style=\"font-size: 90%;\">###DATE###</p><p>".$dsplTitle.$dsplUnderline."</p>";
        // /smh 2009-01-08
       } else {
        // multiple days, whole day
@@ -85,43 +95,40 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
        $gCalDateEnd = date($dateformat, $item['enddate'] - $SECSINDAY); 
        // smh 2009-01-08
        //$event_display="<p style=\"font-size: 90%;\">###DATE### to ###DATEEND###</p><div><strong>###TITLE###</strong>".$dsplUnderline."</div>";
-       $event_display="<p style=\"font-size: 90%;\">###DATE### to ###DATEEND###</p><p><strong>".$dsplTitleLink."</strong>".$dsplUnderline."</p>";
+       $event_display="<p style=\"font-size: 90%;\">###DATE### to ###DATEEND###</p><p>".$dsplTitle.$dsplUnderline."</p>";
        // /smh 2009-01-08
       }
     } else {
        //  Single day, part of day
        // smh 2009-01-08
        //$event_display="<p style=\"font-size: 90%;\">###DATE### ###FROM### - ###UNTIL###</p><div><strong>".$dsplTitleLink."</strong>".$dsplUnderline."</div>";
-       $event_display="<p style=\"font-size: 90%;\">###DATE### ###FROM### - ###UNTIL###</p><p><strong>###TITLE###</strong>".$dsplUnderline."</p>";
+       $event_display="<p style=\"font-size: 90%;\">###DATE### ###FROM### - ###UNTIL###</p><p>.$dsplTitle.$dsplUnderline."</p>";
        // /smh 2009-01-08
     }
     // /smh 2008-12-17
     
     //Make any URLs used in the description also clickable: thanks Adam
-    $item['description'] = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item['description']);
+    $desc = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item->get_description());
 
     // Now, let's run it through some str_replaces, and store it with the date for easy sorting later
     $temp_event=$event_display;
-    $temp_dateheader=$event_dateheader;
-    $temp_event=str_replace("###TITLE###",$item['title'],$temp_event);
-    $temp_event=str_replace("###DESCRIPTION###",$item['description'],$temp_event);
-    $temp_event=str_replace("###DATE###",$gCalDate,$temp_event);
+    $temp_event=str_replace("###TITLE###",$item->get_title(),$temp_event);
+    $temp_event=str_replace("###DESCRIPTION###",$desc,$temp_event);
+    $temp_event=str_replace("###DATE###",$startDate,$temp_event);
     // smh: 2008-12-17 - We need to decode the End Date of an event sometimes..
     $temp_event=str_replace("###DATEEND###",$gCalDateEnd,$temp_event);
     // /smh: 2008-12-17
-    $temp_dateheader=str_replace("###DATE###",$gCalDate,$temp_dateheader);
-    $temp_event=str_replace("###FROM###",$gCalStartTime,$temp_event);
-    $temp_event=str_replace("###UNTIL###",$gCalEndTime,$temp_event);
-    $temp_event=str_replace("###WHERE###",$item['where'],$temp_event);
-    $temp_event=str_replace("###BACKLINK###",$item['backlink'],$temp_event);
-    $temp_event=str_replace("###LINK###",$item['link'],$temp_event);
-    $temp_event=str_replace("###MAPLINK###","http://maps.google.com/?q=".urlencode($item['where']),$temp_event);
+    $temp_event=str_replace("###FROM###",$startTime,$temp_event);
+    $temp_event=str_replace("###UNTIL###",$endTime,$temp_event);
+    $temp_event=str_replace("###WHERE###",$item->get_location(),$temp_event);
+    $temp_event=str_replace("###BACKLINK###",urldecode(JURI::base().'index.php?option=com_gcalendar&task=event&eventID='.$item->get_id().'&calendarName='.$calName.'&ctz='.$tz),$temp_event);
+    $temp_event=str_replace("###LINK###",$item->get_link(),$temp_event);
+    $temp_event=str_replace("###MAPLINK###","http://maps.google.com/?q=".urlencode($item->get_location()),$temp_event);
     // Accept and translate HTML
     $temp_event=str_replace("&lt;","<",$temp_event);
     $temp_event=str_replace("&gt;",">",$temp_event);
     $temp_event=str_replace("&quot;","\"",$temp_event);
 
-	if ($GroupByDate) {if ($gCalDate!=$old_date) { echo $temp_dateheader; $old_date=$gCalDate;}}
-	echo $temp_event;
+        echo $temp_event;
 }
 ?>
