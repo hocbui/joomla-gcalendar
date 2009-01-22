36f6a43bc6f55198b8b0e1ce145a7aafbb5cd8e8
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2009-01-22 10:46:38 +0000

    Issue 107: the output prints now what is necessary. this means when it is a full day just the dates and not the time, etc.

--- Joomla_1_5_x/mod_gcalendar_latest/tmpl/default.php
+++ Joomla_1_5_x/mod_gcalendar_latest/tmpl/default.php
@@ -20,39 +20,13 @@ $SECSINDAY=86400;
 //
 // How you want each thing to display.
 // All bits listed below which are available:
-// ###TITLE###, ###DESCRIPTION###, ###DATE###, ###PUBLISHED###
+// ###TITLE###, ###DESCRIPTION###, ###PUBLISHEDDATE###, ###PUBLISHEDTIME###, 
+// ###STARTDATE###, ###STARTTIME###, ###DATESEPARATOR###, ###ENDDATE###, ###ENDTIME###
 // ###WHERE###, ###BACKLINK###, ###LINK###, ###MAPLINK###
 $dsplLink = "<a href='###BACKLINK###'>###TITLE###</a>";
 if($params->get( 'openWindow', 0 )==1)
 	$dsplLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
-$event_display="<p>".JText::_("PUBLISHED")." ###PUBLISHEDDATE### ###PUBLISHEDTIME###<br>###DATE### ###FROM###<br>".$dsplLink."</p>";
-
-// smh 2009-01-09 Added params for underline between events and if link is active
-$dsplUnderline = '';
-if($params->get( 'showUnderline', 0) == 1){
-   $dsplUnderline = "<HR>";
-}
-// dsplTitleLink determines if the Title will be clickable or not
-$dsplTitleLink = "###TITLE###";
-if ($params->get( 'showLink', 1)==1) {
-   if($params->get( 'openWindow', 0 )==1) {
-        $dsplTitleLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
-   } else {
-        $dsplTitleLink = "<a href='###BACKLINK###'>###TITLE###</a>";
-   }     
-}
-// Added option for a bolded title
-$dsplTitle = $dsplTitleLink;
-if ($params->get( 'boldTitle', 0)==1) {
-   $dsplTitle = "<strong>".$dsplTitleLink."</strong>";  
-}
-
-$dsplFontPerc = 100;
-if ($params->get( 'fontPerc', "" ) != "") {
-   $dsplFontPerc = $params->get( 'fontPerc', "" );  
-}
-
-// /smh 2009-01-09
+$event_display="<p>".JText::_("PUBLISHED")." ###PUBLISHEDDATE### ###PUBLISHEDTIME###<br>###STARTDATE### ###STARTTIME### ###DATESEPARATOR### ###ENDDATE### ###ENDTIME###<br>".$dsplLink."</p>";
 
 // Date format you want your details to appear
 $dateformat=$params->get('dateFormat', 'd.m.Y'); // 10 March 2009 - see http://www.php.net/date for details
@@ -72,9 +46,12 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
 	// These are the dates we'll display
     $startDate = date($dateformat, $item->get_start_time());
     $startTime = date($timeformat, $item->get_start_time());
+    $endDate = date($dateformat, $item->get_end_time());
     $endTime = date($timeformat, $item->get_end_time());
     $pubDate = date($dateformat, $item->get_publish_date());
     $pubTime = date($timeformat, $item->get_publish_date());
+    
+    $temp_event=$event_display;
     
     // smh 2008-12-17
     //       - modification to allow flexible output for various event types viz:
@@ -83,53 +60,47 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
     //       Multiple days - whole day
     //   N.B.  This formatting is fixed for now.  We need to add params to allow configuration in future.
     
-    $gCalDateEnd = date($dateformat, $item->get_end_time());
     
-    // Now customise display format based on event as part of day, whole day or multiple days
-    // Need to know if it is whole days or not.  Google reports this with end date > start date
-    if ($item->get_start_time < $item->get_end_time()) {
-      // For a single whole of day, Google reports the end date as the next day
-      //  So, we check to see if start date + 1 day = end day (i.e. a one day, whole day event)
-      if (($item->get_start_time()+ $SECSINDAY) == $item->get_end_time()) {
-         // Single day, whole day	
-       // smh 2009-01-08
-         //$event_display="<p style=\"font-size: 90%;\">###DATE###</p><div><strong>###TITLE###</strong>".$dsplUnderline."</div>";
-         $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">".JText::_("PUBLISHED")." ###PUBLISHEDDATE### ###PUBLISHEDTIME###<br>###DATE###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-       // /smh 2009-01-08
-      } else {
-       // multiple days, whole day
-       // So, bring end date back to real date. 
-       $gCalDateEnd = date($dateformat, $item->get_end_time() - $SECSINDAY); 
-       // smh 2009-01-08
-       //$event_display="<p style=\"font-size: 90%;\">###DATE### to ###DATEEND###</p><div><strong>###TITLE###</strong>".$dsplUnderline."</div>";
-       $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">".JText::_("PUBLISHED")." ###PUBLISHEDDATE### ###PUBLISHEDTIME###<br>###DATE### to ###DATEEND###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-       // /smh 2009-01-08
-      }
-    } else {
-       //  Single day, part of day
-       // smh 2009-01-08
-       //$event_display="<p style=\"font-size: 90%;\">###DATE### ###FROM### - ###UNTIL###</p><div><strong>".$dsplTitleLink."</strong>".$dsplUnderline."</div>";
-       $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">".JText::_("PUBLISHED")." ###PUBLISHEDDATE### ###PUBLISHEDTIME###<br>###DATE### ###FROM### - ###UNTIL###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-       // /smh 2009-01-08
-    }
-    // /smh 2008-12-17
+	// Now customise display format based on event as part of day, whole day or multiple days
+	// Need to know if it is whole days or not.  Google reports this with end date > start date
+	if ($item->get_start_time < $item->get_end_time()) {
+		// For a single whole of day, Google reports the end date as the next day
+		//  So, we check to see if start date + 1 day = end day (i.e. a one day, whole day event)
+		if (($item->get_start_time()+ $SECSINDAY) == $item->get_end_time()) {
+		    // Single day, whole day
+		   	$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+			$temp_event=str_replace("###STARTTIME###","",$temp_event);
+			$temp_event=str_replace("###DATESEPARATOR###","",$temp_event);
+			$temp_event=str_replace("###ENDDATE###","",$temp_event);
+			$temp_event=str_replace("###ENDTIME###","",$temp_event);
+	  	} else {
+			// multiple days, whole day
+			// So, bring end date back to real date. 
+			$endDate = date($dateformat, $item->get_end_time() - $SECSINDAY);
+			$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+			$temp_event=str_replace("###STARTTIME###","",$temp_event);
+			$temp_event=str_replace("###DATESEPARATOR###",JText::_("DATE_SEPARATOR"),$temp_event); 
+			$temp_event=str_replace("###ENDDATE###",$endDate,$temp_event);
+			$temp_event=str_replace("###ENDTIME###","",$temp_event);
+		}
+	} else {
+		//  Single day, part of day
+		$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+		$temp_event=str_replace("###STARTTIME###",$startTime,$temp_event);
+		$temp_event=str_replace("###DATESEPARATOR###",JText::_("DATE_SEPARATOR"),$temp_event); 
+		$temp_event=str_replace("###ENDDATE###","",$temp_event);
+		$temp_event=str_replace("###ENDTIME###",$endTime,$temp_event);
+	}
+	// /smh 2008-12-17
     
     
     //Make any URLs used in the description also clickable
     $desc = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item->get_description());
 
-    // Now, let's run it through some str_replaces, and store it with the date for easy sorting later
-    $temp_event=$event_display;
     $temp_event=str_replace("###TITLE###",$item->get_title(),$temp_event);
     $temp_event=str_replace("###DESCRIPTION###",$desc,$temp_event);
     $temp_event=str_replace("###PUBLISHEDDATE###",$pubDate,$temp_event);
     $temp_event=str_replace("###PUBLISHEDTIME###",$pubTime,$temp_event);
-    $temp_event=str_replace("###DATE###",$startDate,$temp_event);
-    // smh: 2008-12-17 - We need to decode the End Date of an event sometimes..
-    $temp_event=str_replace("###DATEEND###",$gCalDateEnd,$temp_event);
-    // /smh: 2008-12-17
-    $temp_event=str_replace("###FROM###",$startTime,$temp_event);
-    $temp_event=str_replace("###UNTIL###",$endTime,$temp_event);
     $temp_event=str_replace("###WHERE###",$item->get_location(),$temp_event);
     $temp_event=str_replace("###BACKLINK###",urldecode(JURI::base().'index.php?option=com_gcalendar&task=event&eventID='.$item->get_id().'&calendarName='.$calName.'&ctz='.$tz),$temp_event);
     $temp_event=str_replace("###LINK###",$item->get_link(),$temp_event);
--- Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
+++ Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
@@ -20,39 +20,14 @@ $SECSINDAY=86400;
 //
 // How you want each thing to display.
 // All bits listed below which are available:
-// ###TITLE###, ###DESCRIPTION###, ###DATE###, ###FROM###, ###UNTIL###,
+// ###TITLE###, ###DESCRIPTION###, 
+// ###STARTDATE###, ###STARTTIME###, ###DATESEPARATOR###, ###ENDDATE###, ###ENDTIME###
 // ###WHERE###, ###BACKLINK###, ###LINK###, ###MAPLINK###
 // You can put ###DATE### in here too if you want to, and disable the 'group by date' below.
 $dsplLink = "<a href='###BACKLINK###'>###TITLE###</a>";
 if($params->get( 'openWindow', 0 )==1)
         $dsplLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
-$event_display="<p>###DATE### ###FROM###<br>".$dsplLink."</p>";
-
-// smh 2009-01-08 Added params for underline between events and if link is active
-$dsplUnderline = "";
-if ($params->get( 'showUnderline', 0 ) == 1){
-   $dsplUnderline = "<HR>";
-}
-//// dsplTitleLink determines if the Title will be clickable or not
-$dsplTitleLink = "###TITLE###";
-if ($params->get( 'showLink', 1 )==1) {
-   if($params->get( 'openWindow', 0 )==1) {
-        $dsplTitleLink = "<a href='###LINK###' target='_blank'>###TITLE###</a>";
-   } else {
-        $dsplTitleLink = "<a href='###BACKLINK###'>###TITLE###</a>";
-   } 
-}
-// Added option for a bolded title
-$dsplTitle = $dsplTitleLink;
-if ($params->get( 'boldTitle', 0 )==1) {
-   $dsplTitle = "<strong>".$dsplTitleLink."</strong>";  
-}
-
-$dsplFontPerc = 100;
-if ($params->get( 'fontPerc', "" ) != "") {
-   $dsplFontPerc = $params->get( 'fontPerc', "" );  
-}
-// /smh 2009-01-08
+$event_display="<p>###STARTDATE### ###STARTTIME### ###DATESEPARATOR### ###ENDDATE### ###ENDTIME###<br>".$dsplLink."</p>";
 
 // Date format you want your details to appear
 $dateformat=$params->get('dateFormat', 'd.m.Y'); // 10 March 2009 - see http://www.php.net/date for details
@@ -61,18 +36,21 @@ $calName = $params->get( 'name', NULL );
 
 // Loop through the array, and display what we wanted.
 for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++){
-        $item = $gcalendar_data[$i];
-        
-        $tz = $params->get('timezone', '');
-        if($tz == ''){
-                $feed = $item->get_feed();
-                $tz = $feed->get_timezone();
-        }
+    $item = $gcalendar_data[$i];
+    
+    $tz = $params->get('timezone', '');
+    if($tz == ''){
+            $feed = $item->get_feed();
+            $tz = $feed->get_timezone();
+    }
 
-        // These are the dates we'll display
+    // These are the dates we'll display
     $startDate = date($dateformat, $item->get_start_time());
     $startTime = date($timeformat, $item->get_start_time());
+    $endDate = date($dateformat, $item->get_end_time());
     $endTime = date($timeformat, $item->get_end_time());
+    
+    $temp_event=$event_display;
     
     // smh 2008-12-17
     //       - modification to allow flexible output for various event types viz:
@@ -81,41 +59,43 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
     //       Multiple days - whole day
     //   N.B.  This formatting is fixed for now.  We need to add params to allow configuration in future.
     
-    $gCalDateEnd = date($dateformat, $item->get_end_time());
-    
-    // Now customise display format based on event as part of day, whole day or multiple days
-    // Need to know if it is whole days or not.  Google reports this with end date > start date
-    if ($item->get_start_time() < $item->get_end_time()) {
-      // For a single whole of day, Google reports the end date as the next day
-      //  So, we check to see if start date + 1 day = end day (i.e. a one day, whole day event)
-      if (($item->get_start_time()+ $SECSINDAY) == $item->get_end_time()) {
-         // Single day, whole day	
-         $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">###DATE###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-      } else {
-       // multiple days, whole day
-       // So, bring end date back to real date. 
-       $gCalDateEnd = date($dateformat, $item->get_end_time() - $SECSINDAY); 
-       $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">###DATE### to ###DATEEND###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-      }
-    } else {
-       //  Single day, part of day
-       $event_display="<p style=\"font-size: ".$dsplFontPerc."%;\">###DATE### ###FROM### - ###UNTIL###</p><p>".$dsplTitle.$dsplUnderline."</p>";
-    }
-    // /smh 2008-12-17
+	// Now customise display format based on event as part of day, whole day or multiple days
+	// Need to know if it is whole days or not.  Google reports this with end date > start date
+	if ($item->get_start_time() < $item->get_end_time()) {
+		// For a single whole of day, Google reports the end date as the next day
+		//  So, we check to see if start date + 1 day = end day (i.e. a one day, whole day event)
+		if (($item->get_start_time()+ $SECSINDAY) == $item->get_end_time()) {
+			// Single day, whole day	
+			$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+			$temp_event=str_replace("###STARTTIME###","",$temp_event);
+			$temp_event=str_replace("###DATESEPARATOR###","",$temp_event);
+			$temp_event=str_replace("###ENDDATE###","",$temp_event);
+			$temp_event=str_replace("###ENDTIME###","",$temp_event);
+		} else {
+			// multiple days, whole day
+			// So, bring end date back to real date. 
+			$endDate = date($dateformat, $item->get_end_time() - $SECSINDAY); 
+			$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+			$temp_event=str_replace("###STARTTIME###","",$temp_event);
+			$temp_event=str_replace("###DATESEPARATOR###",JText::_("DATE_SEPARATOR"),$temp_event); 
+			$temp_event=str_replace("###ENDDATE###",$endDate,$temp_event);
+			$temp_event=str_replace("###ENDTIME###","",$temp_event);
+		}
+	} else {
+		//  Single day, part of day
+		$temp_event=str_replace("###STARTDATE###",$startDate,$temp_event);
+		$temp_event=str_replace("###STARTTIME###",$startTime,$temp_event);
+		$temp_event=str_replace("###DATESEPARATOR###",JText::_("DATE_SEPARATOR"),$temp_event); 
+		$temp_event=str_replace("###ENDDATE###","",$temp_event);
+		$temp_event=str_replace("###ENDTIME###",$endTime,$temp_event);
+	}
+	// /smh 2008-12-17
     
     //Make any URLs used in the description also clickable: thanks Adam
     $desc = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item->get_description());
 
-    // Now, let's run it through some str_replaces, and store it with the date for easy sorting later
-    $temp_event=$event_display;
     $temp_event=str_replace("###TITLE###",$item->get_title(),$temp_event);
     $temp_event=str_replace("###DESCRIPTION###",$desc,$temp_event);
-    $temp_event=str_replace("###DATE###",$startDate,$temp_event);
-    // smh: 2008-12-17 - We need to decode the End Date of an event sometimes..
-    $temp_event=str_replace("###DATEEND###",$gCalDateEnd,$temp_event);
-    // /smh: 2008-12-17
-    $temp_event=str_replace("###FROM###",$startTime,$temp_event);
-    $temp_event=str_replace("###UNTIL###",$endTime,$temp_event);
     $temp_event=str_replace("###WHERE###",$item->get_location(),$temp_event);
     $temp_event=str_replace("###BACKLINK###",urldecode(JURI::base().'index.php?option=com_gcalendar&task=event&eventID='.$item->get_id().'&calendarName='.$calName.'&ctz='.$tz),$temp_event);
     $temp_event=str_replace("###LINK###",$item->get_link(),$temp_event);
@@ -125,6 +105,6 @@ for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++)
     $temp_event=str_replace("&gt;",">",$temp_event);
     $temp_event=str_replace("&quot;","\"",$temp_event);
 
-        echo $temp_event;
+	echo $temp_event;
 }
 ?>
