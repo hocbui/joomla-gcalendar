b917570bfaa89d68928240fd19f8bce3c39b7eae
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2009-05-21 14:12:54 +0000

    tagged with version 2.1.0 RC1

---
+++ tags/V_2_1_0_RC1/Joomla_1_5_x/com_gcalendar/admin/libraries/rss-calendar/GCalendar.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2009 Allon Moritz
+ * @version $Revision: 2.1.0 $
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+require_once (JPATH_ADMINISTRATOR.DS.'components'.DS.'com_gcalendar'.DS.'util.php');
+require_once ('classes/EventRenderer.php');
+require_once ('classes/CalendarRenderer.php');
+
+class GCalendar {
+	var $cal;
+	var $today, $month, $year, $day, $view;
+	var $mainFilename;
+	var $config;
+
+	function GCalendar($feedFetcher, $config) {
+		GCalendarUtil::ensureSPIsLoaded();
+		$this->config = $config;
+		$this->mainFilename = "index.php?option=com_gcalendar&view=gcalendar";
+		$this->today = getdate();
+
+		$this->month = isset($_GET["month"]) ? $_GET["month"] : $this->today["mon"];
+		$this->year = isset($_GET["year"]) ? $_GET["year"] : $this->today["year"];
+		$this->day = isset($_GET["day"]) ? $_GET["day"] : $this->today["mday"];
+		if (!checkdate($this->month, $this->day, $this->year)) {
+			$this->day = 1;
+		}
+
+		$this->view = isset($_GET["gcalendarview"]) ? $_GET["gcalendarview"] : $config['defaultView'];
+		if(isset($config['forceView'])){
+			$this->view = $config['forceView'];
+		}
+
+		$this->year = (int)$this->year;
+		$this->month = (int)$this->month;
+		$this->day = (int)$this->day;
+
+		if (isset($_SERVER['HTTP_USER_AGENT'])) {
+			$uaRaw = strtolower($_SERVER['HTTP_USER_AGENT']);
+			$this->uaVersion = "unk";
+			if (strpos($uaRaw, "opera") !== false)
+			$this->userAgent = "opera";
+			elseif (strpos($uaRaw, "msie") !== false) {
+				$this->userAgent = "ie";
+				if (strpos($uaRaw, "msie 6") !== false) $this->uaVersion = 6;
+			}
+			else
+			$this->userAgent = "other";
+
+			if (strpos($uaRaw, "mac") !== false)
+			$this->uaPlatform = "mac";
+			else
+			$this->uaPlatform = "other";
+		}
+		else {
+			$this->uaVersion = "unk";
+			$this->userAgent = "unk";
+			$this->uaPlatform = "unk";
+		}
+		switch($this->view) {
+			case "month":
+				$start = mktime(0, 0, 0, $this->month, 1, $this->year);
+				$end = strtotime( "+1 month", $start );
+				break;
+			case "day":
+				$start = mktime(0, 0, 0, $this->month, $this->day, $this->year);
+				$end = strtotime( "+1 day", $start );
+				break;
+			case "week":
+				$start = CalendarRenderer::getFirstDayOfWeek($this->year, $this->month, $this->day, $config['weekStart']);
+				$end = strtotime( "+1 week +1 day", $start );
+		}
+		$this->feeds = $feedFetcher->getGoogleCalendarEvents($start, $end, $config['projection']);
+		$this->cal = new CalendarRenderer($this);
+	}
+
+	function getFeeds(){
+		return $this->feeds;
+	}
+
+	function display() {
+		$document =& JFactory::getDocument();
+		JHTML::_('behavior.modal');
+		$document->addScript('administrator/components/com_gcalendar/libraries/nifty/nifty.js');
+		$document->addStyleSheet('administrator/components/com_gcalendar/libraries/nifty/niftyCorners.css');
+		$document->addStyleSheet('administrator/components/com_gcalendar/libraries/rss-calendar/gcalendar.css');
+		if ($this->userAgent == "ie") {
+			$document->addStyleSheet('administrator/components/com_gcalendar/libraries/rss-calendar/gcalendar-ie6.css');
+		}
+
+		$cal = &$this->cal;
+
+		$year = &$this->year;
+		$month = &$this->month;
+		$day = &$this->day;
+		$view = &$this->view;
+		echo "<div class=\"gcalendar\">\n";
+
+		if($this->config['showSelectionList'] == 'yes'){
+			$this->printCalendarSelectionList($year, $month, $day, $view);
+		}
+
+		if($this->config['showToolbar'] == 'yes'){
+			$this->printToolBar($year, $month, $day, $view);
+		}
+		$cal->printCal($year, $month, $day, $view);
+		echo "</div>\n";
+	}
+
+	function printCalendarSelectionList(){
+		JHTML::_('behavior.mootools');
+		$document = &JFactory::getDocument();
+		$document->addScript( 'administrator/components/com_gcalendar/libraries/rss-calendar/gcalendar.js' );
+		$calendar_list = "<div id=\"gc_gcalendar_view_list\"><table>\n";
+		$feeds = $this->feeds;
+		if(!empty($feeds)){
+			foreach($feeds as $feed){
+				$calendar_list .="<tr>\n";
+				$calendar_list .="<td><div class=\"gccal_".$feed->get('gcid')."\"><font color=\"#FFFFFF\">".$feed->get('gcname')."</font></div></td></tr>\n";
+			}
+		}
+		$calendar_list .="</table></div>\n";
+		echo $calendar_list;
+		echo "<div align=\"center\" style=\"text-align:center\">\n";
+		echo "<a id=\"gc_gcalendar_view_toggle\" name=\"gc_gcalendar_view_toggle\" href=\"#\">\n";
+		echo "<img id=\"gc_gcalendar_view_toggle_status\" name=\"gc_gcalendar_view_toggle_status\" src=\"".JURI::base()."administrator/components/com_gcalendar/libraries/rss-calendar/img/btn-down.png\"/>\n";
+		echo "</a></div>\n";
+	}
+
+	function printToolBar($year, $month, $day, $view){
+		$cal = &$this->cal;
+		// Generate URLs for next/prev buttons
+		switch($view) {
+			case "month":
+				$nextMonth = ($month == 12) ? 1 : $month+1;
+				$prevMonth = ($month == 1) ? 12 : $month-1;
+				$nextYear = ($month == 12) ? $year+1 : $year;
+				$prevYear = ($month == 1) ? $year-1 : $year;
+				$prevURL = $this->mainFilename . "&gcalendarview=month&year=${prevYear}&month=${prevMonth}";
+				$nextURL = $this->mainFilename . "&gcalendarview=month&year=${nextYear}&month=${nextMonth}";
+				break;
+			case "week":
+				list($nextYear, $nextMonth, $nextDay) = explode(",", date("Y,n,j", strtotime("+7 days", strtotime("${year}-${month}-${day}"))));
+				list($prevYear, $prevMonth, $prevDay) = explode(",", date("Y,n,j", strtotime("-7 days", strtotime("${year}-${month}-${day}"))));
+
+				$prevURL = $this->mainFilename . "&gcalendarview=week&year=${prevYear}&month=${prevMonth}&day=${prevDay}";
+				$nextURL = $this->mainFilename . "&gcalendarview=week&year=${nextYear}&month=${nextMonth}&day=${nextDay}";
+
+				break;
+			case "day":
+				list($nextYear, $nextMonth, $nextDay) = explode(",", date("Y,n,j", strtotime("+1 day", strtotime("${year}-${month}-${day}"))));
+				list($prevYear, $prevMonth, $prevDay) = explode(",", date("Y,n,j", strtotime("-1 day", strtotime("${year}-${month}-${day}"))));
+
+				$prevURL = $this->mainFilename . "&gcalendarview=day&year=${prevYear}&month=${prevMonth}&day=${prevDay}";
+				$nextURL = $this->mainFilename . "&gcalendarview=day&year=${nextYear}&month=${nextMonth}&day=${nextDay}";
+
+				break;
+			}
+
+			$document =& JFactory::getDocument();
+			$calCode  = "function datePickerClosed(dateField){\n";
+			$calCode .= "var gcdateValues = dateField.value.split('/');\n";
+			$calCode .= "var gcformatValues = '".$this->config['dateFormat']."'.split('/');\n";
+			$calCode .= "var gcday = '';\n";
+			$calCode .= "var gcmonth = '';\n";
+			$calCode .= "var gcyear = '';\n";
+			$calCode .= "for(i = 0; i < gcformatValues.length; i++){\n";
+			$calCode .= "if(gcformatValues[i]=='dd')\n";
+			$calCode .= "gcday = gcdateValues[i];\n";
+			$calCode .= "else if(gcformatValues[i]=='mm')\n";
+			$calCode .= "gcmonth = gcdateValues[i];\n";
+			$calCode .= "else if(gcformatValues[i]=='yy')\n";
+			$calCode .= "gcyear = gcdateValues[i];\n";
+			$calCode .= "}\n";
+			$calCode .= "document.getElementById('gc_go_link').href = '".JRoute::_($this->mainFilename."&gcalendarview=".$view)."&day='+gcday+'&month='+gcmonth+'&year='+gcyear;\n";
+			$calCode .= "}\n";
+			$document->addScriptDeclaration($calCode);
+
+			$document->addScript('administrator/components/com_gcalendar/libraries/jquery/jquery-1.3.2.js');
+			$document->addScript('administrator/components/com_gcalendar/libraries/jquery/ui/ui.core.js');
+			$document->addScript('administrator/components/com_gcalendar/libraries/jquery/ui/ui.datepicker.js');
+			$document->addStyleSheet('administrator/components/com_gcalendar/libraries/jquery/themes/redmond/ui.all.css');
+
+			$calCode = "jQuery.noConflict();\n";
+			$calCode .= "jQuery(document).ready(function(){\n";
+			$calCode .= "document.getElementById('gcdate').value = jQuery.datepicker.formatDate('".$this->config['dateFormat']."', new Date(".$year.", ".$month." - 1, ".$day."));\n";
+			$calCode .= "jQuery(\"#gcdate\").datepicker({dateFormat: '".$this->config['dateFormat']."'});\n";
+			$calCode .= "})\n";
+			$document->addScriptDeclaration($calCode);
+
+			echo "<div id=\"calToolbar\">\n";
+			echo "<div id=\"calPager\" class=\"Item\">\n";
+			echo "<a class=\"Item\" href=\"".JRoute::_($prevURL)."\" title=\"previous ".$view."\">\n";
+			$this->image("btn-prev.gif", "previous ".$view, "prevBtn_img");
+			echo "</a>\n";
+			echo "<span class=\"ViewTitle Item\">\n";
+			$cal->printViewTitle($year, $month, $day, $view);
+			echo "</span>\n";
+			echo "<a class=\"Item\" href=\"".JRoute::_($nextURL)."\" title=\"next ".$view."\">\n";
+			$this->image("btn-next.gif", "next ".$view, "nextBtn_img");
+			echo "</a></div>\n";
+			echo "<a class=\"Item\" href=\"".JRoute::_($this->mainFilename."&gcalendarview=".$view."&year=".$this->today["year"]."&month=".$this->today["mon"]."&day=".$this->today["mday"])."\">\n";
+			$this->image("btn-today.gif", "go to today", "", "today_img");
+			echo "</a>\n";
+			echo "<input class=\"Item\"	type=\"text\" name=\"gcdate\" id=\"gcdate\" \n";
+			echo "onchange=\"datePickerClosed(this);\" \n";
+			echo "size=\"10\" maxlength=\"10\" title=\"jump to date\" />";
+			echo "<a class=\"Item\" id=\"gc_go_link\" href=\"".JRoute::_($this->mainFilename."&gcalendarview=".$view."&year=".$year."&month=".$month."&day=".$day)."\">\n";
+			$this->image("btn-go.gif", "go to date", "gi_img");
+			echo "</a>\n";
+
+			echo "<div id=\"viewSelector\" class=\"Item\">\n";
+			echo "<a href=\"".JRoute::_($this->mainFilename."&gcalendarview=day&year=".$year."&month=".$month."&day=".$day)."\">\n";
+			$this->image("cal-day.gif", "day view", "calday_img");
+			echo "</a>\n";
+
+			echo "<a href=\"".JRoute::_($this->mainFilename."&gcalendarview=week&year=".$year."&month=".$month."&day=".$day)."\">\n";
+			$this->image("cal-week.gif", "week view", "calweek_img");
+			echo "</a>\n";
+
+			echo "<a href=\"".JRoute::_($this->mainFilename."&gcalendarview=month&year=".$year."&month=".$month."&day=".$day)."\">\n";
+			$this->image("cal-month.gif", "month view", "calmonth_img");
+			echo "</a></div></div>\n";
+	}
+
+	function image($name, $alt = "[needs alt tag]", $id="", $attrs="") {
+		list($width, $height, $d0, $d1) = getimagesize(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_gcalendar'.DS.'libraries'.DS.'rss-calendar'.DS.'img'.DS . $name);
+		echo "<img src=\"".JURI::base() . "administrator/components/com_gcalendar/libraries/rss-calendar/img/" . $name."\"";
+		echo " id=\"". $id."\" width=\"". $width."\" height=\"".$height."\" alt=\"".$alt."\" border=\"0\"";
+		echo $attrs ."/>";
+	}
+}
+?>
---
+++ tags/V_2_1_0_RC1/Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2009 Allon Moritz
+ * @version $Revision: 2.1.0 $
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+if(!empty($error)){
+	echo $error;
+	return;
+}
+
+$SECSINDAY=86400;
+
+$event_display=$params->get('output', '');
+
+// Date format you want your details to appear
+$dateformat=$params->get('dateFormat', '%d.%m.%Y');
+$timeformat=$params->get('timeFormat', '%H:%M');
+$calName = $params->get( 'name', NULL );
+
+// Loop through the array, and display what we wanted.
+for ($i = 0; $i < sizeof($gcalendar_data) && $i <$params->get( 'max', 5 ); $i++){
+	$item = $gcalendar_data[$i];
+
+	$feed = $item->get_feed();
+	$tz = GCalendarUtil::getComponentParameter('timezone');
+	if($tz == ''){
+		$tz = $feed->get_timezone();
+	}
+
+	$itemID = GCalendarUtil::getItemId($feed->get('gcid'));
+	if(!empty($itemID))$itemID = '&Itemid='.$itemID;
+
+	// These are the dates we'll display
+	$startDate = strftime($dateformat, $item->get_start_date());
+	$startTime = strftime($timeformat, $item->get_start_date());
+	$endDate = strftime($dateformat, $item->get_end_date());
+	$endTime = strftime($timeformat, $item->get_end_date());
+
+	$temp_event=$event_display;
+
+	switch($item->get_day_type()){
+		case $item->SINGLE_WHOLE_DAY:
+			$temp_event=str_replace("{startdate}",$startDate,$temp_event);
+			$temp_event=str_replace("{starttime}","",$temp_event);
+			$temp_event=str_replace("{dateseparator}","",$temp_event);
+			$temp_event=str_replace("{enddate}","",$temp_event);
+			$temp_event=str_replace("{endtime}","",$temp_event);
+			break;
+		case $item->SINGLE_PART_DAY:
+			$temp_event=str_replace("{startdate}",$startDate,$temp_event);
+			$temp_event=str_replace("{starttime}",$startTime,$temp_event);
+			$temp_event=str_replace("{dateseparator}","-",$temp_event);
+			$temp_event=str_replace("{enddate}","",$temp_event);
+			$temp_event=str_replace("{endtime}",$endTime,$temp_event);
+			break;
+		case $item->MULTIPLE_WHOLE_DAY:
+			$endDate = strftime($dateformat, $item->get_end_date() - $SECSINDAY);
+			$temp_event=str_replace("{startdate}",$startDate,$temp_event);
+			$temp_event=str_replace("{starttime}","",$temp_event);
+			$temp_event=str_replace("{dateseparator}","-",$temp_event);
+			$temp_event=str_replace("{enddate}",$endDate,$temp_event);
+			$temp_event=str_replace("{endtime}","",$temp_event);
+			break;
+		case $item->MULTIPLE_PART_DAY:
+			$temp_event=str_replace("{startdate}",$startDate,$temp_event);
+			$temp_event=str_replace("{starttime}",$startTime,$temp_event);
+			$temp_event=str_replace("{dateseparator}","-",$temp_event);
+			$temp_event=str_replace("{enddate}",$endDate,$temp_event);
+			$temp_event=str_replace("{endtime}",$endTime,$temp_event);
+			break;
+	}
+
+	//Make any URLs used in the description also clickable: thanks Adam
+	$desc = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?,&//=]+)','<a href="\\1">\\1</a>', $item->get_description());
+
+	$temp_event=str_replace("{title}",$item->get_title(),$temp_event);
+	$temp_event=str_replace("{description}",$desc,$temp_event);
+	$temp_event=str_replace("{where}",$item->get_location(),$temp_event);
+	$temp_event=str_replace("{backlink}",JRoute::_('index.php?option=com_gcalendar&view=event&eventID='.$item->get_id().'&gcid='.$feed->get('gcid').$itemID),$temp_event);
+	$temp_event=str_replace("{link}",$item->get_link().'&ctz='.$tz,$temp_event);
+	$temp_event=str_replace("{maplink}","http://maps.google.com/?q=".urlencode($item->get_location()),$temp_event);
+	$temp_event=str_replace("{calendarname}",$feed->get('gcalendarName'),$temp_event);
+	$temp_event=str_replace("{calendarcolor}",$feed->get('gcalendarColor'),$temp_event);
+	// Accept and translate HTML
+	$temp_event=str_replace("&lt;","<",$temp_event);
+	$temp_event=str_replace("&gt;",">",$temp_event);
+	$temp_event=str_replace("&quot;","\"",$temp_event);
+
+	echo $temp_event;
+}
+?>
