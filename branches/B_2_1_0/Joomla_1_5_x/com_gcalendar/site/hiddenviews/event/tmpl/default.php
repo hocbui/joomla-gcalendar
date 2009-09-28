4e145c3ed1baced445015d5fc31ab532c8d4503a
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2009-09-28 10:36:58 +0000

    Issue 223: added calendar and author email link

--- branches/B_2_1_0/Joomla_1_5_x/com_gcalendar/site/hiddenviews/event/tmpl/default.php
+++ branches/B_2_1_0/Joomla_1_5_x/com_gcalendar/site/hiddenviews/event/tmpl/default.php
@@ -93,7 +93,7 @@ if($event == null){
 	$authors = $event->get_authors();
 	if(count($authors)>0){
 		$document->addScript('components/com_gcalendar/hiddenviews/event/tmpl/default.js');
-		echo "<tr><td class=\"event_content_key\">".JText::_( 'AUTHOR' ).": </td><td><a href=\"javascript:sdafgkl437jeeee('".str_replace('@','#',$authors[0]->get_email())."')\">".$authors[0]->get_name()."</a></td></tr>\n";
+		echo "<tr><td class=\"event_content_key\">".JText::_( 'AUTHOR' ).": </td><td><a href=\"javascript:sdafgkl437jeeee('".base64_encode(str_replace('@','#',$authors[0]->get_email()))."')\">".$authors[0]->get_name()."</a></td></tr>\n";
 	}
 	echo "</table></div>\n";
 }
