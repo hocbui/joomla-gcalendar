a0547b53d3be83c5a61681f05506ec2f0c452797
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2010-05-07 14:26:15 +0000

    Tagged with release 2.2.1

---
+++ tags/V_2_2_1/Joomla_1_5_x/com_gcalendar/site/hiddenviews/jsonfeed/tmpl/module.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2010 Allon Moritz
+ * @since 2.2.0
+ */
+
+defined('_JEXEC') or die('Restricted access');
+$data = array();
+$SECSINDAY=86400;
+
+$startDate = JRequest::getInt('start', null);
+$endDate = JRequest::getInt('end', null);
+$browserTz = JRequest::getInt('browserTimezone', null);
+if(!empty($browserTz))
+$browserTz = $browserTz * -60;
+else
+$browserTz = 0;
+
+$gcalendarOffset = GCalendarModelJSONFeed::getGCalendarTZOffset($startDate);
+$requestedDayStart = $startDate + $browserTz - $gcalendarOffset;
+$requestedDayEnd = $requestedDayStart + $SECSINDAY;
+
+while ($requestedDayStart < $endDate) {
+	$result = array();
+	$linkIDs = '';
+	$description = '';
+	foreach ($this->calendars as $calendar){
+		$calID = null;
+		$items = $calendar->get_items();
+		foreach ($items as $item) {
+			if(($requestedDayStart <= $item->get_start_date() && $item->get_start_date() < $requestedDayEnd)
+			|| ($requestedDayStart < $item->get_end_date() && $item->get_end_date() <= $requestedDayEnd)
+			|| ($item->get_start_date() <= $requestedDayStart && $requestedDayEnd <= $item->get_end_date())){
+				$result[] = $item;
+				$calID = $calendar->get('gcid').',';
+				$description .= '<li><font color="#'.$calendar->get('gccolor').'">'.htmlspecialchars_decode($item->get_title()).'</font></li>';
+			}
+		}
+		if($calID != null)
+		$linkIDs .= $calID;
+	}
+	if(!empty($result)){
+		$linkIDs = trim($linkIDs, ",");
+		$day = strftime('%d', $requestedDayStart);
+		$month = strftime('%m', $requestedDayStart);
+		$year = strftime('%Y', $requestedDayStart);
+		$url = JRoute::_('index.php?option=com_gcalendar&view=day&gcids='.$linkIDs.'#year='.$year.'&month='.$month.'&day='.$day);
+
+		$data[] = array(
+			'id' => time(),
+			'title' => '',
+			'start' => strftime('%Y-%m-%dT%H:%M:%S', $requestedDayStart),
+		//			'end' => $requestedDayEnd - 10,
+			'url' => $url,
+		//			'className' => "gcal-module_event_gccal",
+			'allDay' => true,
+			'description' => sprintf(JText::_('MODULE_TEXT'), count($result)).'<ul>'.$description.'</ul>'
+			);
+	}
+	$requestedDayStart += $SECSINDAY;
+	$requestedDayEnd = $requestedDayStart + $SECSINDAY;
+}
+echo json_encode($data);
+?>
