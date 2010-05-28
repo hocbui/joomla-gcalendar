716b0aafaac4dfa1d06a9e848166b0dba014bc1b
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2010-05-28 09:07:08 +0000

    Issue 301: The yes option shows the colors of the calendar, the custom the colors of the options

--- Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
+++ Joomla_1_5_x/mod_gcalendar_upcoming/tmpl/default.php
@@ -29,21 +29,24 @@ echo $params->get( 'text_before' );
 if(!empty($gcalendar_data)){
 	foreach( $gcalendar_data as $item){
 		// APRIL 2010 MOD - CALENDAR IMAGES by Tyson Moore
-		if($params->get('images', 'no') == 'yes') {
+		if($params->get('images', 'no') != 'no') {
 			$tmp = JFactory::getDate($item->get_start_date());
 			$startDate = $tmp->toFormat($dateformat);
 			$month = $tmp->toFormat('%m');
 			$month_text = strtoupper($tmp->toFormat('%h'));
 			$day = $tmp->toFormat('%e');
-			$monthdiv = '<div style="float: left; margin-right: 6px; width: 42px; height: 42px; background-image: url(\'modules/mod_gcalendar_upcoming/tmpl/images/calendar-icon.gif\');"><div style="background-color: #' . $params->get('calimage_background') . '; width: 32px; height: 10px; margin-top: 6px; margin-left: 5px;"></div><div style="padding:2px; font-weight: bold; font-size: 10px; color: #' . $params->get('calimage_month') . '; text-align: center; position: relative; margin-top: -15px; margin-bottom: -4px;">' . $month_text . '</div>';
-			$daydiv = '<div style="font-weight: bold; font-size: 1.3em; color: #' . $params->get('calimage_day') . '; width: 42px; text-align: center;">' . $day . '</div>';
-			echo $monthdiv;
-			echo $daydiv;
+			$feed = $item->get_feed();
+			$colorImageBackground = $params->get('images', 'yes') == 'custom' ? '#'.$params->get('calimage_background') : GCalendarUtil::getFadedColor($feed->get('gccolor'), 80);
+			$colorMonth = $params->get('images', 'yes') == 'custom' ? $params->get('calimage_month') : 'FFFFFF';
+			$colorDay = $params->get('images', 'yes') == 'custom' ? $params->get('calimage_day') : $feed->get('gccolor');
+			echo '<div style="float: left; margin-right: 6px; width: 42px; height: 42px; background-image: url(\'modules/mod_gcalendar_upcoming/tmpl/images/calendar-icon.gif\');">';
+			echo '<div style="background-color: ' . $colorImageBackground . '; width: 32px; height: 10px; margin-top: 6px; margin-left: 5px;"></div><div style="padding:2px; font-weight: bold; font-size: 10px; color: #' . $colorMonth . '; text-align: center; position: relative; margin-top: -15px; margin-bottom: -4px;">' . $month_text . '</div>';
+			echo '<div style="font-weight: bold; font-size: 1.3em; color: #' . $colorDay . '; width: 42px; text-align: center;">' . $day . '</div>';
 			echo '</div>';
 		}
 		//END MOD
 		echo GCalendarUtil::renderEvent($item, $event_display, $dateformat, $timeformat);
-		if($params->get('images', 'no') == 'yes') {
+		if($params->get('images', 'no') != 'no') {
 			echo '<p style="clear: both;"/>';
 		}
 	}
