3de207eb0150e2c511bddfb67a0bc1674de8dded
tanguy.pruvot <tanguy.pruvot@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2010-09-17 02:29:32 +0000

    fullcalendar IE9 support

--- trunk/Joomla_1_5_x/com_gcalendar/site/views/gcalendar/tmpl/default.php
+++ trunk/Joomla_1_5_x/com_gcalendar/site/views/gcalendar/tmpl/default.php
@@ -185,7 +185,7 @@ if($params->get('show_event_as_popup', 1) == 1){
 	$calCode .= "		           modal: true,\n";
 	$calCode .= "		           autoResize: true,\n";
 	$calCode .= "		           title: event.title\n";
-	$calCode .= "		        }).width(".($popupWidth-20).").height(".($popupHeight-20).");\n";
+	$calCode .= "		        }).outerWidth(".($popupWidth-20).").outerHeight(".($popupHeight-20).");\n";
 	$calCode .= "		        return false;}\n";
 }
 $calCode .= "		},\n";
