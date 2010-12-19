79e9c39e8a64c29cab55b475ffae5bfdcce43aa8
tanguy.pruvot <tanguy.pruvot@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2010-12-19 21:35:25 +0000

    Updated jQuery UI to 1.8.7, and renamed ui css file to remove version

--- Joomla_1_5_x/mod_gcalendar/tmpl/default.php
+++ Joomla_1_5_x/mod_gcalendar/tmpl/default.php
@@ -23,6 +23,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 JHTML::_('behavior.mootools');
 GCalendarUtil::loadJQuery();
 $document = &JFactory::getDocument();
+$document->addScript(JURI::base().'components/com_gcalendar/libraries/jquery/ui/jquery-ui-1.8.7.custom.min.js');
 $document->addScript(JURI::base(). 'components/com_gcalendar/libraries/fullcalendar/fullcalendar.min.js' );
 $document->addStyleSheet(JURI::base().'components/com_gcalendar/libraries/fullcalendar/fullcalendar.css');
 $document->addScript(JURI::base().'components/com_gcalendar/libraries/jquery/ext/jquery.qtip-1.0.0.min.js');
@@ -36,7 +37,7 @@ $theme = $params->get('theme', '');
 if(JRequest::getVar('theme', null) != null)
 $theme = JRequest::getVar('theme', null);
 if(!empty($theme))
-$document->addStyleSheet(JURI::base().'components/com_gcalendar/libraries/jquery/themes/'.$theme.'/jquery-ui-1.8.6.custom.css');
+$document->addStyleSheet(JURI::base().'components/com_gcalendar/libraries/jquery/themes/'.$theme.'/jquery-ui.custom.css');
 
 $daysLong = "[";
 $daysShort = "[";
