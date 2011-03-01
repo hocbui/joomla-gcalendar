9b00485a60b5ab46f9dae6417c1e208baa094c75
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2011-03-01 12:20:20 +0000

    Issue 73: Usiing ACL API in the code

--- trunk/Joomla_1_6_x/com_gcalendar/admin/views/gcalendar/view.html.php
+++ trunk/Joomla_1_6_x/com_gcalendar/admin/views/gcalendar/view.html.php
@@ -4,15 +4,15 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * GCalendar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @author Allon Moritz
  * @copyright 2007-2010 Allon Moritz
  * @since 2.2.0
@@ -37,16 +37,48 @@ class GCalendarsViewGCalendar extends JView
 	{
 		//get the calendar
 		$gcalendar	=& $this->get('Data');
-		$isNew		= ($gcalendar->id < 1);
+		$isNew		= $gcalendar->id < 1;
 
 		$text = $isNew ? JText::_( 'New' ) : JText::_( 'Edit' );
 		JToolBarHelper::title(   JText::_( 'GCALENDAR_MANAGER' ).': <small><small>[ ' . $text.' ]</small></small>' ,'calendar');
-		JToolBarHelper::save();
-		if ($isNew)  {
-			JToolBarHelper::cancel();
-		} else {
-			// for existing items the button is renamed `close`
-			JToolBarHelper::cancel( 'cancel', 'Close' );
+		//		JToolBarHelper::save();
+		//		if ($isNew)  {
+		//			JToolBarHelper::cancel();
+		//		} else {
+		//			// for existing items the button is renamed `close`
+		//			JToolBarHelper::cancel( 'cancel', 'Close' );
+		//		}
+
+		JRequest::setVar('hidemainmenu', true);
+		$canDo = GCalendarUtil::getActions($gcalendar->id);
+		if ($isNew){
+			if ($canDo->get('core.create')){
+				JToolBarHelper::apply('helloworld.apply', 'JTOOLBAR_APPLY');
+				JToolBarHelper::save('helloworld.save', 'JTOOLBAR_SAVE');
+				JToolBarHelper::custom('helloworld.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+			}
+			JToolBarHelper::cancel('cancel', 'JTOOLBAR_CANCEL');
+		}else{
+			if ($canDo->get('core.edit')){
+				// We can save the new record
+				JToolBarHelper::apply('helloworld.apply', 'JTOOLBAR_APPLY');
+				JToolBarHelper::save('helloworld.save', 'JTOOLBAR_SAVE');
+
+				// We can save this record, but check the create permission to see if we can return to make a new one.
+				if ($canDo->get('core.create')){
+					JToolBarHelper::custom('helloworld.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+				}
+			}
+			if ($canDo->get('core.create')){
+				JToolBarHelper::custom('helloworld.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
+			}
+			JToolBarHelper::cancel('helloworld.cancel', 'JTOOLBAR_CLOSE');
+		}
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))){
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
 		}
 
 		$this->assignRef('gcalendar', $gcalendar);
