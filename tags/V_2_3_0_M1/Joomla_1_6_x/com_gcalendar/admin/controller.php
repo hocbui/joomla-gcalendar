1baf27f2e5b01bb2749c0d7c2b7d962481fb7d90
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2011-03-08 12:17:32 +0000

    Tagged as version 2.3.0 M1

---
+++ tags/V_2_3_0_M1/Joomla_1_6_x/com_gcalendar/admin/controller.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2010 Allon Moritz
+ * @since 2.2.0
+ */
+
+jimport('joomla.application.component.controller');
+
+/**
+ * GCalendar Component Controller
+ *
+ */
+class GCalendarsController extends JController
+{
+
+	function display()
+	{
+		parent::display();
+
+		JSubMenuHelper::addEntry(JText::_('COM_GCALENDAR_SUBMENU_GCALENDARS'), 'index.php?option=com_gcalendar', $submenu == 'gcalendars');
+		JSubMenuHelper::addEntry(JText::_('COM_GCALENDAR_SUBMENU_TOOLS'), 'index.php?option=com_gcalendar&view=tools', $submenu == 'tools');
+		JSubMenuHelper::addEntry(JText::_('COM_GCALENDAR_SUBMENU_SUPPORT'), 'index.php?option=com_gcalendar&view=support', $submenu == 'support');
+
+		$document = JFactory::getDocument();
+		$document->addStyleDeclaration('.icon-48-calendar {background-image: url(../media/com_gcalendar/images/48-calendar.png);background-repeat: no-repeat;}');
+	}
+
+	function import(){
+		if($this->isLoggedIn()){
+			JRequest::setVar( 'view', 'import'  );
+		}else{
+			JRequest::setVar( 'nextTask', 'import'  );
+			JRequest::setVar( 'view', 'login'  );
+		}
+		JRequest::setVar('hidemainmenu', 0);
+
+		$this->display();
+	}
+
+	function isLoggedIn(){
+		global $_SESSION, $_GET;
+		if (!isset($_SESSION['sessionToken']) && !isset($_GET['token'])
+		&& !isset($_SESSION['authToken']) && !isset($_GET['authtoken']) ) {
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+}
+?>
