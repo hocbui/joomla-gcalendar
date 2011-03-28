dd2651bbef49330960f94a6988ca3940974e299b
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2011-03-28 18:49:24 +0000

    Tagged as version 2.3.0 M3

---
+++ tags/V_2_3_0_M3/Joomla_1_6_x/com_gcalendar/admin/tables/gcalendar.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2011 Allon Moritz
+ * @since 2.2.0
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.database.table');
+
+class GCalendarTableGCalendar extends JTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param object Database connector object
+	 */
+	function __construct(&$db) 
+	{
+		parent::__construct('#__gcalendar', 'id', $db);
+	}
+	/**
+	 * Overloaded bind function
+	 *
+	 * @param       array           named array
+	 * @return      null|string     null is operation was satisfactory, otherwise returns an error
+	 * @see JTable:bind
+	 * @since 1.5
+	 */
+	public function bind($array, $ignore = '') 
+	{
+		if (isset($array['params']) && is_array($array['params'])) 
+		{
+			// Convert the params field to a string.
+			$parameter = new JRegistry;
+			$parameter->loadArray($array['params']);
+			$array['params'] = (string)$parameter;
+		}
+		return parent::bind($array, $ignore);
+	}
+
+	/**
+	 * Overloaded load function
+	 *
+	 * @param       int $pk primary key
+	 * @param       boolean $reset reset data
+	 * @return      boolean
+	 * @see JTable:load
+	 */
+	public function load($pk = null, $reset = true) 
+	{
+		if (parent::load($pk, $reset)) 
+		{
+			// Convert the params field to a registry.
+			$params = new JRegistry;
+			$params->loadJSON($this->params);
+			$this->params = $params;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	/**
+	 * Method to compute the default name of the asset.
+	 * The default name is in the form `table_name.id`
+	 * where id is the value of the primary key of the table.
+	 *
+	 * @return	string
+	 * @since	1.6
+	 */
+	protected function _getAssetName()
+	{
+		$k = $this->_tbl_key;
+		return 'com_gcalendar.calendar.'.(int) $this->$k;
+	}
+
+	/**
+	 * Method to return the title to use for the asset table.
+	 *
+	 * @return	string
+	 * @since	1.6
+	 */
+	protected function _getAssetTitle()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Get the parent asset id for the record
+	 *
+	 * @return	int
+	 * @since	1.6
+	 */
+	protected function _getAssetParentId()
+	{
+		$asset = JTable::getInstance('Asset');
+		$asset->loadByName('com_gcalendar');
+		return $asset->id;
+	}
+}
+?>
---
+++ tags/V_2_3_0_M3/Joomla_1_6_x/com_gcalendar/admin/views/login/tmpl/default.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2011 Allon Moritz
+ * @since 2.2.0
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+$mainframe = &JFactory::getApplication();
+$absolute_path = $mainframe->getCfg( 'absolute_path' );
+ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . JPATH_COMPONENT . DS . 'libraries');
+
+require_once('Zend' . DS . 'Loader.php');
+Zend_Loader::loadClass('Zend_Gdata_AuthSub');
+Zend_Loader::loadClass('Zend_Gdata_HttpClient');
+Zend_Loader::loadClass('Zend_Gdata_Calendar');
+
+$params = &JComponentHelper::getParams( 'com_gcalendar' );
+$domain = $params->get('google_apps_domain');
+
+$u = JFactory::getURI();
+$next = JRoute::_( $u->toString().'?option=com_gcalendar&task='.JRequest::getCmd('nextTask'));
+$scope = 'http://www.google.com/calendar/feeds/';
+$session = true;
+$secure = false;
+$hd = '';
+if(!empty($domain))
+$hd = '&hd='.$domain;
+$authSubUrl = Zend_Gdata_AuthSub::getAuthSubTokenUri($next, $scope, $secure,
+$session,Zend_Gdata_AuthSub::AUTHSUB_REQUEST_URI);
+?>
+<fieldset><legend>Google safe authentication</legend>
+<a href="<?php echo $authSubUrl.$hd;?>">Please Login to access the calendar data.</a>
+</fieldset>
+<hr/>
+<fieldset><legend>Standard authentication</legend>
+<form action="<?php echo JRoute::_( $u->toString().'?option=com_gcalendar&view=import');?>" method="post">
+	<table>
+	<tr><td>Username:</td><td><input type="text" name="user" size="100"/></td></tr>
+	<tr><td>Password:</td><td><input type="password"name="pass" size="100"/></td></tr>
+	<tr><td><input type="submit" value="Login"/></td><td></td></tr>
+	</table>
+</form>
+</fieldset>
