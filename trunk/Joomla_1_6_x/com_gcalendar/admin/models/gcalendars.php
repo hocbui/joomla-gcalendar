2ecbfb78acfcdd567a8e7280b23c204b156ca584
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2011-05-04 13:25:20 +0000

    Started with translations

--- trunk/Joomla_1_6_x/com_gcalendar/admin/models/gcalendars.php
+++ trunk/Joomla_1_6_x/com_gcalendar/admin/models/gcalendars.php
@@ -18,103 +18,22 @@
  * @since 2.2.0
  */
 
-// Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
 
 jimport( 'joomla.application.component.model' );
 
-/**
- * GCalendar Model
- *
- */
-class GCalendarModelGCalendars extends JModel
+class GCalendarModelGCalendars extends JModelList
 {
-	/**
-	 * Calendars data array
-	 *
-	 * @var array
-	 */
-	var $_data;
 
-	/**
-	 * Items total
-	 * @var integer
-	 */
-	var $_total = null;
-
-	/**
-	 * Pagination object
-	 * @var object
-	 */
-	var $_pagination = null;
-
-
-	function __construct()
+	protected function getListQuery()
 	{
-		parent::__construct();
+		$db = JFactory::getDBO();
+		$query = $db->getQuery(true);
 
-		$mainframe = &JFactory::getApplication();
-		$option = $mainframe->scope;
-		
-		// Get pagination request variables
-		$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
-		$limitstart = $mainframe->getUserStateFromRequest($option.'.limitstart', 'limitstart', 0, 'int');
+		$query->select('*');
 
-		// In case limit has been changed, adjust it
-		$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
-
-		$this->setState('limit', $limit);
-		$this->setState('limitstart', $limitstart);
-	}
-
-
-
-	/**
-	 * Returns the query
-	 * @return string The query to be used to retrieve the rows from the database
-	 */
-	function _buildQuery()
-	{
-		$query = " SELECT * FROM #__gcalendar ";
-
+		$query->from('#__gcalendar');
 		return $query;
 	}
-
-	/**
-	 * Retrieves the calendar data
-	 * @return array Array of objects containing the data from the database
-	 */
-	function getData()
-	{
-		// Lets load the data if it doesn't already exist
-		if (empty( $this->_data ))
-		{
-			$query = $this->_buildQuery();
-			$this->_data = $this->_getList( $query,  $this->getState('limitstart'), $this->getState('limit'));
-		}
-
-		return $this->_data;
-	}
-
-	function getPagination()
-	{
-		// Load the content if it doesn't already exist
-		if (empty($this->_pagination)) {
-			jimport('joomla.html.pagination');
-			$this->_pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
-		}
-		return $this->_pagination;
-	}
-
-	function getTotal()
-	{
-		// Load the content if it doesn't already exist
-		if (empty($this->_total)) {
-			$query = $this->_buildQuery();
-			$this->_total = $this->_getListCount($query);
-		}
-		return $this->_total;
-	}
-
-
 }
+?>
