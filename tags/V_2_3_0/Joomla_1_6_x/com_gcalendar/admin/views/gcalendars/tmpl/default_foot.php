ed6977c260ed1467530cb1efbd11039d20b0f943
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2011-08-09 18:08:31 +0000

    Tagged as version 2.3.0

---
+++ tags/V_2_3_0/Joomla_1_6_x/com_gcalendar/admin/views/gcalendars/tmpl/default_foot.php
@@ -0,0 +1,12 @@
+<?php
+defined('_JEXEC') or die('Restricted Access');
+?>
+<tr>
+	<td colspan="5">
+		<?php echo $this->pagination->getListFooter(); ?>
+		<br/><br/>
+		<div align="center">
+			&copy;&nbsp;&nbsp;2011 <a href="http://g4j.laoneo.net" target="_blank">allon moritz</a>
+		</div>
+	</td>
+</tr>
---
+++ tags/V_2_3_0/Joomla_1_6_x/com_gcalendar/admin/views/login/tmpl/default.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * GCalendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GCalendar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GCalendar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Allon Moritz
+ * @copyright 2007-2011 Allon Moritz
+ * @since 2.2.0
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+$mainframe = &JFactory::getApplication();
+$absolute_path = $mainframe->getCfg( 'absolute_path' );
+ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . JPATH_COMPONENT . DS . 'libraries');
+
+require_once('Zend' . DS . 'Loader.php');
+Zend_Loader::loadClass('Zend_Gdata_AuthSub');
+Zend_Loader::loadClass('Zend_Gdata_HttpClient');
+Zend_Loader::loadClass('Zend_Gdata_Calendar');
+
+$params = &JComponentHelper::getParams( 'com_gcalendar' );
+$domain = $params->get('google_apps_domain');
+
+$u = JFactory::getURI();
+$next = JRoute::_( $u->toString().'&task='.JRequest::getCmd('nextTask'));
+$scope = 'http://www.google.com/calendar/feeds/';
+$session = true;
+$secure = false;
+$hd = '';
+if(!empty($domain))
+$hd = '&hd='.$domain;
+$authSubUrl = Zend_Gdata_AuthSub::getAuthSubTokenUri($next, $scope, $secure,
+$session,Zend_Gdata_AuthSub::AUTHSUB_REQUEST_URI);
+?>
+<fieldset><legend><?php echo JText::_('COM_GCALENDAR_VIEW_LOGIN_AUTH_SAFE_LABEL');?></legend>
+<a href="<?php echo $authSubUrl.$hd;?>"><?php echo JText::_('COM_GCALENDAR_VIEW_LOGIN_AUTH_SAFE_DESC');?></a>
+</fieldset>
+<hr/>
+<fieldset><legend><?php echo JText::_('COM_GCALENDAR_VIEW_LOGIN_AUTH_DEFAULT_LABEL');?></legend>
+<form action="<?php echo JRoute::_( $u->toString().'&view=import');?>" method="post">
+	<table>
+	<tr><td><?php echo JText::_('COM_GCALENDAR_VIEW_LOGIN_AUTH_DEFAULT_FIELD_NAME');?>:</td><td><input type="text" name="user" size="100"/></td></tr>
+	<tr><td><?php echo JText::_('COM_GCALENDAR_VIEW_LOGIN_AUTH_DEFAULT_FIELD_PASSWORD');?>:</td><td><input type="password"name="pass" size="100"/></td></tr>
+	<tr><td><input type="submit" value="Login"/></td><td></td></tr>
+	</table>
+</form>
+</fieldset>
