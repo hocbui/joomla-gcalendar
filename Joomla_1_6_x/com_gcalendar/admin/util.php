17115884eaad5f479a1296b0d6c6e8f4faff0929
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-01-17 19:57:20 +0000

    Issue 238: Migrated modules

--- Joomla_1_6_x/com_gcalendar/admin/util.php
+++ Joomla_1_6_x/com_gcalendar/admin/util.php
@@ -189,7 +189,7 @@ class GCalendarUtil{
 		$temp_event=str_replace("{title}",$event->getTitle(),$temp_event);
 		$temp_event=str_replace("{description}",$desc,$temp_event);
 		$temp_event=str_replace("{where}",$event->getLocation(),$temp_event);
-		$temp_event=str_replace("{backlink}",htmlentities(JRoute::_('index.php?option=com_gcalendar&view=event&eventID='.$event->getId().'&gcid='.$event->getParam('gcid').$itemID)),$temp_event);
+		$temp_event=str_replace("{backlink}",htmlentities(JRoute::_('index.php?option=com_gcalendar&view=event&eventID='.$event->getGCalId().'&gcid='.$event->getParam('gcid').$itemID)),$temp_event);
 		$temp_event=str_replace("{link}",$event->getLink().'&ctz='.$tz,$temp_event);
 		$temp_event=str_replace("{maplink}","http://maps.google.com/?q=".urlencode($event->getLocation()),$temp_event);
 		$temp_event=str_replace("{calendarname}",$event->getParam('gcname'),$temp_event);
