a48c0524c2a9b2f933c9f3b3e3f0c217627be92e
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-01-30 19:53:46 +0000

    Issue 72: Access rights are working in the backend

--- GCalendar/com_gcalendar/admin/sql/updates/mysql/2.6.0.sql
+++ GCalendar/com_gcalendar/admin/sql/updates/mysql/2.6.0.sql
@@ -1,3 +1,3 @@
 -- ACL access fields
-ALTER TABLE `#__gcalendar` ADD `access` TINYINT UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `#__gcalendar` ADD `access_content` TINYINT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `#__gcalendar` ADD `access` TINYINT UNSIGNED NOT NULL DEFAULT '1';
+ALTER TABLE `#__gcalendar` ADD `access_content` TINYINT UNSIGNED NOT NULL DEFAULT '1';
