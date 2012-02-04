896eb92a0c4cd284ee7508b67073e882b03d5e3c
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-02-04 09:24:36 +0000

    Added upgrade workaround

--- trunk/GCalendar/com_gcalendar/script.php
+++ trunk/GCalendar/com_gcalendar/script.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * GCalendar is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,52 +21,39 @@
 defined('_JEXEC') or die('Restricted access');
 
 class Com_GCalendarInstallerScript{
-	
-	function install($parent){
-	?>
-	    <div style="margin:0px auto; text-align:center; width:360px;">
-	      <img src="../media/com_gcalendar/images/48-calendar.png" alt="GCalendar Logo" />
-	      <h3 class="headline oktext">GCalendar was installed successfully.</h3>
-	      <p>Open GCalendar manager:</p>
-	      <div class="button2-left" style="margin-left:70px;">
-	        <div class="blank">
-	          <a title="Start" onclick="location.href='index.php?option=com_gcalendar';" href="#">Start now!</a>
-	        </div>
-	      </div>
-	      <div class="button2-left jg_floatright" style="margin-right:70px;">
-	        <div class="blank">
-	          <a title="Languages" onclick="location.href='index.php?option=com_gcalendar&view=support';" href="#">Support</a>
-	        </div>
-	      </div>
-	      <div style="clear:both;"></div>
-	    </div>
-	 <?php
+
+	public function install($parent){
 	}
-	
+
 	function update($parent){
-	?>
-	    <div style="margin:0px auto; text-align:center; width:360px;">
-	      <img src="../media/com_gcalendar/images/48-calendar.png" alt="GCalendar Logo" />
-	      <h3 class="headline oktext">GCalendar was updated successfully.</h3>
-	      <p>Open GCalendar manager:</p>
-	      <div class="button2-left" style="margin-left:70px;">
-	        <div class="blank">
-	          <a title="Start" onclick="location.href='index.php?option=com_gcalendar';" href="#">Start now!</a>
-	        </div>
-	      </div>
-	      <div class="button2-left jg_floatright" style="margin-right:70px;">
-	        <div class="blank">
-	          <a title="Languages" onclick="location.href='index.php?option=com_gcalendar&view=support';" href="#">Support</a>
-	        </div>
-	      </div>
-	      <div style="clear:both;"></div>
-	    </div>
-	 <?php
+		$version = $this->getParam('version');
+		if(version_compare($version, '2.6.0')){
+			$this->run("ALTER TABLE `#__gcalendar` ADD `access` TINYINT UNSIGNED NOT NULL DEFAULT '1';");
+			$this->run("ALTER TABLE `#__gcalendar` ADD `access_content` TINYINT UNSIGNED NOT NULL DEFAULT '1';");
+			$this->run("ALTER TABLE `#__gcalendar` ADD `username` VARCHAR( 255 ) NULL DEFAULT NULL AFTER `magic_cookie`;");
+			$this->run("ALTER TABLE `#__gcalendar` ADD `password` text NULL DEFAULT NULL AFTER `username`;");
+		}
 	}
-	
-	function uninstall($parent){}
-	
-	function preflight($type, $parent){}
-	
-	function postflight($type, $parent){}
+
+	public function uninstall($parent){
+	}
+
+	public function preflight($type, $parent){
+	}
+
+	public function postflight($type, $parent){
+	}
+
+	private function run($query){
+		$db =& JFactory::getDBO();
+		$db->setQuery($query);
+		$db->query();
+	}
+
+	private function getParam( $name ) {
+		$db = JFactory::getDbo();
+		$db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_gcalendar"');
+		$manifest = json_decode( $db->loadResult(), true );
+		return $manifest[ $name ];
+	}
 }
