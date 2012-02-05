01d65cfc1d532ff89e35bb706fce43e585f8a84d
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-02-05 18:56:57 +0000

    Moved to apply the access level outside of the cache

--- trunk/GCalendar/com_gcalendar/admin/util.php
+++ trunk/GCalendar/com_gcalendar/admin/util.php
@@ -338,5 +338,17 @@ class GCalendarUtil{
 
 		return $result;
 	}
+
+	// http://core.trac.wordpress.org/browser/trunk/wp-includes/formatting.php#L1461
+	public static function transformUrl( $text ) {
+		$ret = ' ' . $ret;
+		// in testing, using arrays here was found to be faster
+		$ret = preg_replace_callback('#([\s>])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is', '_make_url_clickable_cb', $ret);
+		$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is', '_make_web_ftp_clickable_cb', $ret);
+		$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_make_email_clickable_cb', $ret);
+		// this one is not in an array because we need it to run last, for cleanup of accidental links within links
+		$ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
+		$ret = trim($ret);
+		return $ret;
+	}
 }
-?>
