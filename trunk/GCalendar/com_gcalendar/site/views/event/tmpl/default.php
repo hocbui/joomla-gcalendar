e6884709dd9fdb0e26faa8675b870f151bcdedb7
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-04-23 14:08:07 +0000

    Started to move all the event rendering into one single function in the util class, some params are now broken

--- trunk/GCalendar/com_gcalendar/site/views/event/tmpl/default.php
+++ trunk/GCalendar/com_gcalendar/site/views/event/tmpl/default.php
@@ -20,8 +20,6 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-require_once(JPATH_COMPONENT.DS.'libraries'.DS.'mustache'.DS.'Mustache.php');
-
 $dispatcher = JDispatcher::getInstance();
 JPluginHelper::importPlugin('gcalendar');
 
@@ -30,123 +28,8 @@ $document->addStyleSheet(JURI::base(). 'components/com_gcalendar/views/gcalendar
 $document->addStyleSheet(JURI::base().'components/com_gcalendar/views/event/tmpl/default.css');
 $document->addScript(JURI::base().'components/com_gcalendar/views/event/tmpl/default.js');
 
-$event = $this->event;
-
-$m = new Mustache;
-
-$variables = array();
-$variables['event'] = $event != null;
-
-$itemID = GCalendarUtil::getItemId(JRequest::getVar('gcid', null));
-if(!empty($itemID) && JRequest::getVar('tmpl', null) != 'component' && $event != null){
-	$component = JComponentHelper::getComponent('com_gcalendar');
-	$menu = JFactory::getApplication()->getMenu();
-	$item = $menu->getItem($itemID);
-	if($item !=null){
-		$backLinkView = $item->query['view'];
-		$dateHash = '';
-		if($backLinkView == 'gcalendar'){
-			$day = strftime('%d', $event->getStartDate());
-			$month = strftime('%m', $event->getStartDate());
-			$year = strftime('%Y', $event->getStartDate());
-			$dateHash = '#year='.$year.'&month='.$month.'&day='.$day;
-		}
-	}
-	$variables['calendarLink'] = JRoute::_('index.php?option=com_gcalendar&Itemid='.$itemID.$dateHash);
-}
-
-// the date formats from http://php.net/date
-$dateformat = GCalendarUtil::getComponentParameter('event_date_format', 'm.d.Y');
-$timeformat = GCalendarUtil::getComponentParameter('event_time_format', 'g:i a');
-
-// These are the dates we'll display
-$startDate = GCalendarUtil::formatDate($dateformat, $event->getStartDate());
-$startTime = GCalendarUtil::formatDate($timeformat, $event->getStartDate());
-$endDate = GCalendarUtil::formatDate($dateformat, $event->getEndDate());
-$endTime = GCalendarUtil::formatDate($timeformat, $event->getEndDate());
-$dateSeparator = '-';
-
-$timeString = $startTime.' '.$startDate.' '.$dateSeparator.' '.$endTime.' '.$endDate;
-$copyDateTimeFormat = 'Ymd';
-switch($event->getDayType()){
-	case GCalendar_Entry::SINGLE_WHOLE_DAY:
-		$timeString = $startDate;
-		$copyDateTimeFormat = 'Ymd';
-		break;
-	case GCalendar_Entry::SINGLE_PART_DAY:
-		$timeString = $startDate.' '.$startTime.' '.$dateSeparator.' '.$endTime;
-		$copyDateTimeFormat = 'Ymd\THis';
-		break;
-	case GCalendar_Entry::MULTIPLE_WHOLE_DAY:
-		$SECSINDAY=86400;
-		$endDate = GCalendarUtil::formatDate($dateformat, $event->getEndDate()-$SECSINDAY);
-		$timeString = $startDate.' '.$dateSeparator.' '.$endDate;
-		$copyDateTimeFormat = 'Ymd';
-		break;
-	case GCalendar_Entry::MULTIPLE_PART_DAY:
-		$timeString = $startTime.' '.$startDate.' '.$dateSeparator.' '.$endTime.' '.$endDate;
-		$copyDateTimeFormat = 'Ymd\THis';
-		break;
-}
-$variables['calendarName'] = GCalendarUtil::getComponentParameter('show_calendar_name', 1) == 1 ? $event->getParam('gcname') : null;
-$variables['title'] = GCalendarUtil::getComponentParameter('show_event_title', 1) == 1 ? (string)$event->getTitle() : null;
-$variables['date'] = GCalendarUtil::getComponentParameter('show_event_date', 1) == 1 ? $timeString : null;
-
-if(GCalendarUtil::getComponentParameter('show_event_attendees', 2) == 1 && count($event->getWho()) > 0){
-	$variables['hasAttendees'] = true;
-	$variables['attendees'] = array();
-	foreach ($event->getWho() as $a) {
-		$variables['attendees'][] = array('name' => (string)$a->getValueString(), 'email' =>  base64_encode(str_replace('@','#',$a->getEmail())));
-	}
-}
-$variables['location'] = GCalendarUtil::getComponentParameter('show_event_location', 1) == 1 ? $event->getLocation() : null;
-$variables['map'] = GCalendarUtil::getComponentParameter('show_event_location_map', 1) == 1 ? urlencode($event->getLocation()) : null;
-
-if(GCalendarUtil::getComponentParameter('show_event_description', 1) == 1) {
-	$variables['description'] = $event->getContent();
-	if(GCalendarUtil::getComponentParameter('event_description_format', 1) == 1) {
-		$variables['description'] = preg_replace("@(src|href)=\"https?://@i",'\\1="',$event->getContent());
-		$variables['description'] = nl2br(preg_replace("@(((f|ht)tp:\/\/)[^\"\'\>\s]+)@",'<a href="\\1" target="_blank">\\1</a>', $variables['description']));
-	}
-}
-if(GCalendarUtil::getComponentParameter('show_event_author', 2) == 1){
-	$variables['hasAuthor'] = true;
-	$variables['author'] = array();
-	foreach ($event->getAuthor() as $author) {
-		$variables['author'][] = array('name' => (string)$author->getName(), 'email' =>  base64_encode(str_replace('@','#',$author->getEmail())));
-	}
-}
-
-if(GCalendarUtil::getComponentParameter('show_event_copy_info', 1) == 1){
-	$variables['copyGoogleUrl'] = 'http://www.google.com/calendar/render?action=TEMPLATE&amp;text='.urlencode($event->getTitle());
-	$variables['copyGoogleUrl'] .= '&amp;dates='.GCalendarUtil::formatDate($copyDateTimeFormat, $event->getStartDate()).'%2F'.GCalendarUtil::formatDate($copyDateTimeFormat, $event->getEndDate());
-	$variables['copyGoogleUrl'] .= '&amp;location='.urlencode($event->getLocation());
-	$variables['copyGoogleUrl'] .= '&amp;details='.urlencode($event->getContent());
-	$variables['copyGoogleUrl'] .= '&amp;hl='.GCalendarUtil::getFrLanguage().'&amp;ctz='.GCalendarUtil::getComponentParameter('timezone');
-	$variables['copyGoogleUrl'] .= '&amp;sf=true&amp;output=xml';
-
-	$ical_timeString_start =  $startTime.' '.$startDate;
-	$ical_timeString_start = strtotime($ical_timeString_start);
-	$ical_timeString_end =  $endTime.' '.$endDate;
-	$ical_timeString_end = strtotime($ical_timeString_end);
-	$loc = $event->getLocation();
-	$variables['copyOutlookUrl'] = JRoute::_("index.php?option=com_gcalendar&view=ical&format=raw&start=".$ical_timeString_start."&end=".$ical_timeString_end."&title=".$event->getTitle()."&location=".$loc);
-}
-
-$variables['calendarLinkLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_CALENDAR_BACK_LINK');
-$variables['calendarNameLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_CALENDAR_NAME');
-$variables['titleLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_EVENT_TITLE');
-$variables['dateLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_WHEN');
-$variables['attendeesLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_ATTENDEES');
-$variables['locationLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_LOCATION');
-$variables['descriptionLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_DESCRIPTION');
-$variables['authorLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_AUTHOR');
-$variables['copyLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_COPY');
-$variables['copyGoogleLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_COPY_TO_MY_CALENDAR');
-$variables['copyOutlookLabel'] = JText::_('COM_GCALENDAR_EVENT_VIEW_COPY_TO_MY_CALENDAR_ICS');
-$variables['language'] = substr(GCalendarUtil::getFrLanguage(),0,2);
-
 $content = '
+{{#events}}
 {{#calendarLink}}
 <table class="gcalendar-table">
 	<tr>
@@ -161,7 +44,6 @@ $content = '
 	</tr>
 </table>
 {{/calendarLink}}
-{{#event}}
 <div class="event_content">
 <table id="content_table">
 	<tr><td colspan="2">{{#pluginsBefore}} {{{.}}} {{/pluginsBefore}}</td></tr>
@@ -218,21 +100,18 @@ $content = '
 	<tr><td colspan="2">{{#pluginsAfter}} {{{.}}} {{/pluginsAfter}}</td></tr>
 </table>
 </div>
-{{/event}}
-{{^event}}
+{{/events}}
+{{^events}}
 no event found
-{{/event}}
+{{/events}}
 ';
 
+$variables = array();
 $variables['pluginsBefore'] = array();
 $variables['pluginsAfter'] = array();
- $dispatcher->trigger('onBeforeDisplayEvent', array($event,  &$content, &$variables));
-// $dispatcher->trigger('onAfterDisplayEvent', array($event,  &$content, &$variables));
+$dispatcher->trigger('onBeforeDisplayEvent', array($this->event,  &$content, &$variables));
+$dispatcher->trigger('onAfterDisplayEvent', array($this->event,  &$content, &$variables));
 
-try{
-	echo $m->render($content, $variables);
-}catch(Exception $e){
-	echo $e->getMessage();
-}
+echo GCalendarUtil::renderEventNew(array($this->event), $content, JFactory::getApplication()->getParams(), $variables);
 ?>
 <div style="text-align:center;margin-top:10px" id="gcalendar_powered"><a href="http://g4j.laoneo.net/">Powered by GCalendar</a></div>
