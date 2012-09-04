be7bd4182fd392250846bb64e7bbf21e3dabe1a1
allon.moritz <allon.moritz@b8cb6c82-b026-0410-8aa7-2b2158591c51>
2012-09-04 08:03:02 +0000

    Implemented drag and drop

--- trunk/GCalendar/com_gcalendar/site/views/jsonfeed/tmpl/default.php
+++ trunk/GCalendar/com_gcalendar/site/views/jsonfeed/tmpl/default.php
@@ -23,24 +23,26 @@ defined('_JEXEC') or die('Restricted access');
 $document = JFactory::getDocument();
 $document->setMimeEncoding('application/json');
 
+$dispatcher = JDispatcher::getInstance();
+JPluginHelper::importPlugin('gcalendar');
+
 $data = array();
 $SECSINDAY=86400;
-if(!empty($this->calendars)){
+if (!empty($this->calendars)) {
 	$itemID = JRequest::getVar('Itemid', null);
-	foreach ($this->calendars as $calendar){
+	foreach ($this->calendars as $calendar) {
 		if($itemID == null){
 			$itemID = GCalendarUtil::getItemId($calendar->id);
 		}
 		$params = JFactory::getApplication()->getMenu()->getParams($itemID);
 		$tmp = clone JComponentHelper::getParams('com_gcalendar');
-		if(empty($params)){
+		if (empty($params)) {
 			$params = $tmp;
-		}else{
+		} else {
 			$tmp->merge($params);
 			$params = $tmp;
 		}
 		foreach ($calendar as $event) {
-
 			$dateformat = $params->get('description_date_format', 'm.d.Y');
 			$timeformat = $params->get('description_time_format', 'g:i a');
 
@@ -58,7 +60,7 @@ if(!empty($this->calendars)){
 			$params->set('show_event_author', 1);
 			$params->set('show_event_copy_info', 1);
 
-			if(!empty($itemID)) {
+			if (!empty($itemID)) {
 				$itemID = '&Itemid='.$itemID;
 			} else {
 				$menu = JFactory::getApplication()->getMenu();
@@ -68,18 +70,19 @@ if(!empty($this->calendars)){
 				}
 			}
 			$description = GCalendarUtil::renderEvents(array($event), $params->get('description_format', '{{#events}}<p>{{date}}<br/>{{{description}}}</p>{{/events}}'), $params);
-			if(strlen($description) > 200){
+			if (strlen($description) > 200) {
 				$description = mb_substr($description, 0, 196).' ...';
 			}
 			$allDayEvent = $event->getDayType() == GCalendar_Entry::SINGLE_WHOLE_DAY || $event->getDayType() == GCalendar_Entry::MULTIPLE_WHOLE_DAY;
 
 			$end = $event->getEndDate();
-			if($allDayEvent) {
+			if ($allDayEvent) {
 				$end = clone $event->getEndDate();
 				$end->modify('-1 day');
 			}
-			$data[] = array(
-					'id' => $event->getId(),
+			$eventData = array(
+					'id' => $event->getGCalId(),
+					'gcid' => $event->getParam('gcid'),
 					'title' => htmlspecialchars_decode($event->getTitle()),
 					'start' => $event->getStartDate()->format('Y-m-d\TH:i:s', true),
 					'end' => $end->format('Y-m-d\TH:i:s', true),
@@ -88,6 +91,9 @@ if(!empty($this->calendars)){
 					'allDay' => $allDayEvent,
 					'description' => $description
 			);
+
+			$dispatcher->trigger('onGCEventBeforeLoad', array($event, &$eventData));
+			$data[] = $eventData;
 		}
 	}
 }
